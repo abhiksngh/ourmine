633f002c7cef35a09d06f8c041a2186ef785e51a
RabiTuckman <RabiTuckman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-07-17 21:45:11 +0000

    timing reductions

--- our/workers/worker_reduce.sh
+++ our/workers/worker_reduce.sh
@@ -6,14 +6,22 @@ reduceWorkerTfidf(){
     local maxN=$3
     local incVal=$4
     local outdir=$5
+    local runtimes=$outdir/tfidf_runtimes
+
+    echo "reducer,n,dataset,time" > $runtimes
 
     for((n=$minN;n<=$maxN;n+=$incVal)); do
 	for file in $datadir/*.arff; do
 	    out=`basename $file`
 	    out=${out%.*}
+	    dataset=$out
 	    out=tfidf_n="$n"_$out.arff
 	    echo $out
+	    start=$(date +%s)
 	    $Reducers -tfidf $file $n $outdir/$out
+	    end=$(date +%s)
+	    time=$((end - start))
+	    echo "tfidf,$n,$dataset,$time"
 	done
     done
 } 
@@ -24,14 +32,22 @@ reduceWorkerPCA(){
     local maxN=$3
     local incVal=$4
     local outdir=$5
+    local runtimes=$outdir/pca_runtimes
+
+    echo "reducer,n,dataset,time" > $runtimes
 
     for((n=$minN;n<=$maxN;n+=$incVal)); do
-	for file in $datadir/*.arff; do
+	for file in $datadir/*.arff; do 
 	    out=`basename $file`
 	    out=${out%.*}
+	    dataset=$out
 	    out=pca_n="$n"_$out.arff
 	    echo $out
+	    start=$(date +%s)
 	    reduceViaPCA $file $n $outdir/$out
+	    end=$(date +%s)
+	    time=$((end - start))
+	    echo "pca,$n,$dataset,$time"	   
 	done
     done
 } 
