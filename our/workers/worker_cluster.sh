122036892d5b7544b4b4ba20dfa257ed92ed360b
RabiTuckman <RabiTuckman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-07-19 17:59:58 +0000

    making cluster worker more compliant to multiple machines

--- our/workers/worker_cluster.sh
+++ our/workers/worker_cluster.sh
@@ -1,35 +1,17 @@
 #workers used to cluster with varying k and datasets, and then analyze
 
-#split up the k to run across multiple machines
-clusterKmeansWorker1(){
-    local kay="2 4 8 16 32"
+#split up the k to run across multiple machines using minkK and maxK
+clusterKmeansWorker(){
+    local minK=$1
+    local maxK=$2
+    local incVal=$3
+    local dataDir=$4
     local stats="clusterer,k,dataset,time(seconds)"
     local statsfile=$Save/kmeans_runtimes
     echo $stats >> $statsfile
 
-    for k in $kay; do
-	for file in $Data/sparse/*; do
-	    filename=`basename $file`
-	    filename=${filename%.*}
-	    out=kmeans_k="$k"_$filename.arff 
-	    echo $out
-	    start=$(date +%s.%N)
-	    $Clusterers -k $k $file $Save/$out
-	    end=$(date +%s.%N)
-	    time=$(echo "$end - $start" | bc)
-	    echo "kmeans,$k,$filename,$time" >> $statsfile
-	done
-    done
-}
-
-clusterKmeansWorker2(){
-    local kay="64 128 256"
-    local stats="clusterer,k,dataset,time(seconds)"
-    local statsfile=$Save/kmeans_runtimes
-    echo $stats >> $statsfile
-
-    for k in $kay; do
-	for file in $Data/sparse/*; do
+    for((k=$minK;k<=$maxK;k+=$incVal)); do
+	for file in $dataDir/*.arff; do
 	    filename=`basename $file`
 	    filename=${filename%.*}
 	    out=kmeans_k="$k"_$filename.arff 
@@ -44,13 +26,16 @@ clusterKmeansWorker2(){
 }
 
 clusterGenicWorker(){
-    local kay="2 4 8 16 32 64 128"
+    local minK=$1
+    local maxK=$2
+    local incVal=$3
+    local dataDir=$4
     local stats="clusterer,k,dataset,time(seconds)"
     local statsfile=$Save/genic_runtimes
     echo $stats >> $statsfile
 
-    for k in $kay; do
-	for file in $Data/sparse/*; do
+    for((k=$minK;k<=$maxK;k+=$incVal)); do
+	for file in $dataDir/*.arff; do
 	    filename=`basename $file`
 	    filename=${filename%.*}
 	    out=genic_k="$k"_$filename.arff 
@@ -66,14 +51,17 @@ clusterGenicWorker(){
 
 #this is handled a bit differently, as each canopy's clusters are stored seperately
 clusterCanopyWorker(){
-    local kay="2 4 8 16 32 64 128"
+    local minK=$1
+    local maxK=$2
+    local incVal=$3
+    local dataDir=$4
     local stats="clusterer,k,dataset,time(seconds)"
     local statsfile=$Save/canopy_runtimes
 
     echo $stats >> $statsfile
     
-    for k in $kay; do
-	for file in $Data/sparse/*; do
+    for((k=$minK;k<=$maxK;k+=$incVal)); do
+	for file in $dataDir/*.arff; do
 	    filename=`basename $file`
 	    filename=${filename%.*}
 	    clustdir=$Save/canopy_k="$k"_$filename
@@ -88,11 +76,4 @@ clusterCanopyWorker(){
 	    echo "canopy,$k,$filename,$time" >> $statsfile
 	done
     done
-}
-
-clusterAll(){
-	clusterKmeansWorker1
-	clusterKmeansWorker2
-	clusterGenicWorker
-	clusterCanopyWorker
 }
