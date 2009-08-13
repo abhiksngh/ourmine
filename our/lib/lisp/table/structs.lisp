e1b1d915a5cc5b60c2a9b9b12a62c1f6389e256f
menzies.tim <menzies.tim@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-08-13 22:04:28 +0000

    tables now seem to be working

--- our/lib/lisp/table/structs.lisp
+++ our/lib/lisp/table/structs.lisp
@@ -1,9 +1,11 @@
+;;;; a table stores headers and examples
 (defstruct (table (:print-function table-print))  
-  name
-  columns 
-  class
-  (cautions (make-caution))
-  all)
+  name                      ; symbol          : name of the table
+  columns                   ; list of header  : one header to describe each column of data
+  class                     ; number          : which column is the header column?
+  (cautions (make-caution)) ; list of caution : any load-time errors?
+  all                       ; list of eg      : all the examples
+)
 
 (defun isa (x tbl)  (nth (table-class tbl) x))
 
