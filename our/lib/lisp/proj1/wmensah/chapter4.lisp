3bc2f4cbf7b5fb4cb52ba512800c48d6d19c4e8f
w.w.mensah <w.w.mensah@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-11 17:04:29 +0000

    restored chapter4

--- our/lib/lisp/proj1/wmensah/chapter4.lisp
+++ our/lib/lisp/proj1/wmensah/chapter4.lisp
@@ -3,21 +3,21 @@
 (defun bin-search (obj vec)
   (let ((len (length vec)))
     (and (not (zerop len))
-	 (finder obj vec 0 (- len 1)))))
+         (finder obj vec 0 (- len 1)))))
 
 (defun finder (obj vec start end)
   (let ((range (- end start))) ; range is the number of elements in the vector
     (if (zerop range)   ; if range is 0 then ...
-	(if (eql obj (aref vec start)) ; if obj is the first element then...
-	    obj ; return obj
-	    nil) ; else return nil
-	(let ((mid (+ start (round (/ range 2))))) ; mid = start + (round (range/2))
-	  (let ((obj2 (aref vec mid))) ; obj2 = the middle element in the range
-	    (if (< obj obj2) ; if obj < obj2 then...
-		(finder obj vec start (- mid 1)) ; call finder and have it search from start to (mid - 1)
-		(if (> obj obj2) ; if obj > obj2 then ...
-		    (finder obj vec (+ mid 1) end) ; call finder and have it search from (mid + 1) to end
-		    obj))))))) ; return obj
+        (if (eql obj (aref vec start)) ; if obj is the first element then...
+            obj ; return obj
+            nil) ; else return nil
+        (let ((mid (+ start (round (/ range 2))))) ; mid = start + (round (range/2))
+          (let ((obj2 (aref vec mid))) ; obj2 = the middle element in the range
+            (if (< obj obj2) ; if obj < obj2 then...
+                (finder obj vec start (- mid 1)) ; call finder and have it search from start to (mid - 1)
+                (if (> obj obj2) ; if obj > obj2 then ...
+                    (finder obj vec (+ mid 1) end) ; call finder and have it search from (mid + 1) to end
+                    obj))))))) ; return obj
 
 (deftest test-bin-search ()
   (check
@@ -54,12 +54,12 @@
 (defun mirror? (s)
   (let ((len (length s))) ;len = length of s
     (and (evenp len) ; if the length is even... (can't have palindromes with odd length)
-	 (do ((forward 0 (+ forward 1)) 
-	      (back (- len 1) (- back 1)))
-	     ((or (> forward back) ; if the 2 pointers have crossed paths, ie. forward > back
-		  (not (eql (elt s forward) ; or the element at forward position != element at back position
-			    (elt s back))))
-	      (> forward back)))))) ; nil is returned if either one of these conditions is satisfied
+         (do ((forward 0 (+ forward 1))
+              (back (- len 1) (- back 1)))
+             ((or (> forward back) ; if the 2 pointers have crossed paths, ie. forward > back
+                  (not (eql (elt s forward) ; or the element at forward position != element at back position
+                            (elt s back))))
+              (> forward back)))))) ; nil is returned if either one of these conditions is satisfied
 ; otherwise if the loop ends and none of the given conditions is met, then true is returned.
 
 (deftest test-mirror()
@@ -90,21 +90,21 @@
 (deftest test-position-if()
   (check
     (position-if #'oddp '(2 3 4 5))))
-  
+ 
 
 
 ;; Identifying Tokens
 (defun tokens (str test start)
   (let ((p1 (position-if test str :start start)))
     (if p1
-	(let ((p2 (position-if #'(lambda (c)
-				   (not (funcall test c)))
-			       str :start p1)))
-	  (cons (subseq str p1 p2)
-		(if p2
-		    (tokens str test p2)
-		    nil)))
-	nil)))
+        (let ((p2 (position-if #'(lambda (c)
+                                   (not (funcall test c)))
+                               str :start p1)))
+          (cons (subseq str p1 p2)
+                (if p2
+                    (tokens str test p2)
+                    nil)))
+        nil)))
 
 (defun constituent (c)
   (and (graphic-char-p c)
@@ -117,8 +117,8 @@
 (defun parse-date (str)
   (let ((toks (tokens str #'constituent 0)))
     (list (parse-integer (first toks))
-	  (parse-month (second toks))
-	  (parse-integer (third toks)))))
+          (parse-month (second toks))
+          (parse-integer (third toks)))))
 
 (setq month-names
   #("jan" "feb" "mar" "apr" "may" "jun"
@@ -126,11 +126,13 @@
 
 (defun parse-month (str)
   (let ((p (position str month-names
-		     :test #'string-equal)))
+                     :test #'string-equal)))
     (if p
-	(+ p 1)
-	nil)))
+        (+ p 1)
+        nil)))
 
 (deftest test-parse-date()
   (check
     (parse-date "03 Sep 2009")))
+
+
