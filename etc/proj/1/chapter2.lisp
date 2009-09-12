44a342daeb7fbf585f71fafa0250c8ad2cd80df3
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-12 20:01:02 +0000

    Added 2.4, deftests at 12.

--- etc/proj/1/chapter2.lisp
+++ etc/proj/1/chapter2.lisp
@@ -4,3 +4,10 @@
   (check (equalp
     '(A B C)
     (list 'A 'B 'C))))
+
+;;; 2.4 List Operations
+
+(deftest listops () 
+  (check (equalp
+    '(A B C)
+    (cons (car '(A nil)) (cdr '(nil B C)))))
