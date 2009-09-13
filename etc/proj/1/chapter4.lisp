a55bb4b554f869098a0f31a1a658765c832fa6ac
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-13 23:18:47 +0000

    Added 4.2, deftests at 14.

--- etc/proj/1/chapter4.lisp
+++ etc/proj/1/chapter4.lisp
@@ -1,3 +1,26 @@
 ;;; 4.2 Binary Search [Required]
 
+(defun binsearch (item vector &optional (start 0) (end (length vector)))
+  (let ((pivot (round (/ (+ start end) 2))))
+    (if (and (listp vector) (> (length vector) 0))
+      (if (equalp item (nth pivot vector))
+        pivot
+        (if (not (zerop (- start end)))
+          (if (> item (nth pivot vector))
+            (binsearch item vector pivot end)
+            (binsearch item vector start pivot)
+          )
+          nil
+        )
+      )
+      nil
+    )
+  )
+)
+
+(deftest testbinsearch ()
+  (check (equalp
+    2
+    (binsearch 3 '(1 2 3 4 5 6 7 8)))))
+
 ;;; 4.5 Parsing Dates [Required]
