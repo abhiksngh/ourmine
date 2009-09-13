28f6de5779ef92f3e9e1245ee8543222e6be0157
zackhutzell <zackhutzell@gmail.com>
2009-09-13 00:22:22 +0000

    Added tests for Hyperpipes

--- lib/lisp/learn/nb.lisp
+++ lib/lisp/learn/nb.lisp
@@ -42,25 +42,56 @@
                 classification class) )))
     classification))
 
+(load "tests/data/audiology.lisp")
+(load "tests/data/kr-vs-kp.lisp")
+(load "tests/data/primary-tumor.lisp")
+(load "tests/data/vehicle.lisp")
+(load "tests/data/vote.lisp")
+(load "tests/data/weather.nominal.lisp")
+(load "tests/data/weather2.lisp")
+(load "tests/data/weathernumerics.lisp")
+
+(defun hyperpipes-test ()
+  (hyperpipes (audiology) (audiology))
+  (hyperpipes (kr-vs-kp) (kr-vs-kp))
+  (hyperpipes (primary-tumor) (primary-tumor))
+  (hyperpipes (vehicle) (vehicle))
+  (hyperpipes (vote) (vote))
+  (hyperpipes (weather.nominal) (weather.nominal))
+  (hyperpipes (weather2) (weather2))
+  (hyperpipes (weather-numerics) (weather-numerics)))
+
+;Uses the instances in train to build the seen array and then
+;uses the instances in test to test the classifier.
+(defun hyperpipes (train test &key (stream t))
+  (format stream "~a~%" (table-name train))
+  (multiple-value-bind (seen-array max-array min-array) (hyperpipes-train train)
+    (dolist (instance (table-all test))
+      (let* ((got (hyperpipes-most-contained test instance seen-array max-array min-array))
+             (want (eg-class instance))
+             (success (eql got want)))
+        (format stream "~a ~a ~a~%"  got want (if success "   " "<--"))))))
+
 (defun hyperpipes-train (tbl)
   (let* ((instances (table-all tbl))
          (classi (table-class tbl))
          (seen-array (create-seen-array tbl))
-         (max-array (create-numeric-array tbl))
-         (min-array (create-numeric-array tbl)))
+         (max-array (create-numeric-array tbl most-negative-fixnum))
+         (min-array (create-numeric-array tbl most-positive-fixnum)))
     (dolist (instance instances)  ;; loop thru the instances
       (let* ((features (eg-features instance))
              (instance-class (nth (table-class tbl) features)))
-       (doitems (feature i features)  ;; loop thru the attribute values for an instance
-          (unless (= classi i)
-            (unless (unknownp feature)
-              (cond ((numericp feature) ;; handle numeric attributes
-                      (if (> feature (get-numeric-value tbl max-array instance-class i))
-                          (set-numeric-array tbl max-array instance-class i feature))
-                      (if (< feature (get-numeric-value tbl min-array instance-class i))
-                          (set-numeric-array tbl min-array instance-class i feature)))
-                    (t  ;; handle enum attributes
-                      (set-seen-array tbl seen-array instance-class i feature))))))))
+        (doitems (feature i features)  ;; loop thru the attribute values for an instance
+          (let ((column-name (header-name (nth i (table-columns tbl)))))
+            (unless (= classi i)
+              (unless (unknownp feature)
+                (cond ((numericp column-name) ;; handle numeric attributes
+                        (if (> feature (get-numeric-value tbl max-array instance-class i))
+                            (set-numeric-array tbl max-array instance-class i feature))
+                        (if (< feature (get-numeric-value tbl min-array instance-class i))
+                            (set-numeric-array tbl min-array instance-class i feature)))
+                      (t  ;; handle enum attributes
+                        (set-seen-array tbl seen-array instance-class i feature)))))))))
     (values seen-array max-array min-array)))
 
 (defun hyperpipes-most-contained (tbl instance seen-array max-array min-array)
@@ -69,9 +100,9 @@
         (classes (klasses tbl))
         (classi (table-class tbl))
         (features (eg-features instance)))
-    (dolist (class classes)
+    (dolist (class classes) ;; loop thru the classes
       (let ((count 0))
-        (doitems (feature i features)
+        (doitems (feature i features) ;; loop thru the attribute values for an instance
           (unless (= classi i)
             (unless (unknownp feature)
               (setf count (+ count (hyperpipes-contains tbl seen-array max-array min-array class i feature))))))
@@ -82,7 +113,7 @@
     what))
 
 (defun hyperpipes-contains (tbl seen-array max-array min-array klass i value)
-  (cond ((numericp value)
+  (cond ((numericp (header-name (nth i (table-columns tbl))))
           (if (and (>= (get-numeric-value tbl max-array klass i) value) 
                    (<= (get-numeric-value tbl min-array klass i) value))
               1
@@ -92,6 +123,7 @@
               1
               0))))
 
+;; Creates an array whose dimensions are number of classes X number of attribute columns X max number of attribute values
 (defun create-seen-array (tbl)
   (let ((nclasses (length (klasses tbl)))
         (nattr (table-width tbl))
@@ -101,27 +133,32 @@
           (setf value-count-max (max (length (discrete-uniques col)) value-count-max))))
     (make-array (list nclasses nattr value-count-max) :initial-element 0)))
 
+;; Records a value in the seen array
 (defun set-seen-array (tbl seen-array klass i value)
   (setf (aref seen-array (position klass (klasses tbl))
                          i
                          (position value (discrete-uniques (nth i (table-columns tbl)))))
         1))
 
+;; Returns a value from the seen array
 (defun get-seen-value (tbl seen-array klass i value)
   (aref seen-array (position klass (klasses tbl))
                          i
                          (position value (discrete-uniques (nth i (table-columns tbl))))))
 
-(defun create-numeric-array (tbl)
+;; Creates an array for storing min or max values 
+(defun create-numeric-array (tbl initial-value)
   (let ((nclasses (length (klasses tbl)))
         (nattr (table-width tbl)))
-    (make-array (list nclasses nattr) :initial-element 0)))
+    (make-array (list nclasses nattr) :initial-element initial-value)))
 
+;; Sets a value in a min or max array
 (defun set-numeric-array (tbl numeric-array klass i value)
   (setf (aref numeric-array (position klass (klasses tbl))
                             i)
         value))
 
+;; Returns a value from a min or max array
 (defun get-numeric-value (tbl numeric-array klass i)
   (aref numeric-array (position klass (klasses tbl)) i))
 
@@ -169,9 +206,4 @@
   (defun self-simple-test-nb ()
     (nb-simple  (make-weather egs) 
                 (make-weather egs)))
-
-  (defun hyperpipes-test ()
-    (let* ((tbl (make-weather egs)))
-      (multiple-value-bind (seen-array max-array min-array) (hyperpipes-train tbl)
-        (hyperpipes-most-contained tbl (car (table-all tbl)) seen-array max-array min-array))))
 )
