74f58f80652df62870ef793dbbda41380c37b520
eliflesher <eliflesher@gmail.com>
2009-09-14 01:02:15 +0000

    Commit 4.5 and 4.6. Deftest count upped to 20, I believe.

--- etc/proj/1/chapter4.lisp
+++ etc/proj/1/chapter4.lisp
@@ -23,6 +23,31 @@
     2
     (binsearch 3 '(1 2 3 4 5 6 7 8)))))
 
-;;; 4.5 Parsing Dates [Required]
+;;; 4.5 Parsing Based on Test[Required]
+(deftest tokenremoval ()
+  (check
+     (equal (removenonalpha "eli123abc..d" 
+		#'alpha-char-p 0) 
+		'("eli" "abc" "d"))))
+
+  (defun removenonalpha (str test start)
+    (let ((p1 (position-if test str :start start)))
+      (if p1
+       (let ((p2 (position-if #'(lambda (c)
+                                  (not (funcall test c)))
+                              str :start p1)))
+         (cons (subseq str p1 p2)
+               (if p2
+                   (tokens str test p2)
+                   nil)))
+       nil)))
 
 ;;; 4.6 Structures [Required]
+(defstruct item
+  price
+  name)
+
+(deftest test-structure ()
+   (setf p (make-item :price 5 :name "Chicken"))
+   (check
+    (equal (point-price) 5)))
