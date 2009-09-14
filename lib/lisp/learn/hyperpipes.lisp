1b0f2e214ba7e232abae9ae82973b3ab4f9c40d0
kelcecil <kelcecil@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-14 03:46:54 +0000

    non-commented hyperpipes and one test dataset

--- lib/lisp/learn/hyperpipes.lisp
+++ lib/lisp/learn/hyperpipes.lisp
@@ -1,74 +1,97 @@
-(defun nb (train test &key (stream t))
-  (let* ((acc 0)
-	(all  (table-all test))
-	(max  (length all)))
-    (dolist (one all (/ acc max))
-      (let* ((got     (bayes-classify (eg-features one) (xindex train)))
-	     (want    (eg-class one))
+(load "learn/hungarian-14-heart-disease")
+(load "learn/vote")
+
+(defun hp (train test &key (stream t))
+  (let* (
+        (all  (table-all test))
+        (max-array (buildMinMaxArr train))
+        (min-array (buildMinMaxArr train))
+        (seen-array (buildSeenArr train)))
+    (hp-train train min-array max-array seen-array)
+    (dolist (instance all)
+      (let* ((got     (hp-mostContained test instance min-array max-array seen-array))
+	     (want    (eg-class instance))
 	     (success (eql got want)))
-	(incf acc (if success 1.0 0.0))
-	(format stream "~a ~a ~a ~a~%"  got want  
-		(round (* 100 (/ acc max)))
-		(if success "   " "<--"))))))
+	(format stream "~a ~a ~a ~%"  got want
+                (if success "   " "<--"))))))
 
-(defun bayes-classify (one tbl &optional (m 2) (k 1))
-  (let* ((classes        (klasses tbl))
-         (nclasses       (nklasses tbl))
-         (n              (f        tbl))
-	 (classi         (table-class tbl))
-         (like           most-negative-fixnum)
-         (classification (first classes)))
-    (dolist (class classes)
-      (let* ((prior (/ (+ (f tbl class) k)
-                       (+  n (* k nclasses))))
-             (tmp   (log prior)))
-        (doitems (feature i one)
-          (unless (= classi i)
-            (unless (unknownp feature)
-              (let ((delta (/ (+ (f tbl class i feature)
-                                 (* m prior))
-                              (+ (f tbl class) m))))
-                (incf tmp (log delta))))))
-        (when (> tmp like)
-          (setf like tmp
-                classification class))))
-    classification))
+(defun hp-train (tbl min-array max-array seen-array)
+  (let ((all-instances (table-all tbl))
+        (classi (table-class tbl)))
+     (dolist (per-instance all-instances)
+       (let* ((all-features (eg-features per-instance))
+              (per-instance-class (nth (table-class tbl) all-features)))
+         (doitems (per-feature i all-features)
+           (let ((col-title (header-name (nth i (table-columns tbl)))))
+             (unless (= classi i)
+               (unless (unknownp per-feature)
+                 (cond ((numericp col-title)
+                       (if (> per-feature (grabMinMaxValue tbl max-array per-instance-class i))
+                           (setMinMaxValue tbl max-array per-instance-class i per-feature))
+                       (if (< per-feature (grabMinMaxValue tbl min-array per-instance-class i))
+                           (setMinMaxValue tbl min-array per-instance-class i per-feature)))
+                       (t (setSeenArr tbl seen-array per-instance-class i per-feature)))))))))))
 
-  (defun make-weather (eg)
-    (data :name    'weather 
-	  :columns '(forecast temp humidity windy play)  
-	  :egs      eg))
-  
-(let 
-    ((egs     '((sunny    hot  high   FALSE skip) 
-		(sunny    hot  high   TRUE  skip)
-		(rainy    cool normal TRUE  skip)     
-		(sunny    mild high   FALSE skip)
-		(overcast cool normal TRUE  play)
-		(overcast hot  high   FALSE play)
-		(rainy    mild high   FALSE play)
-		(rainy    cool normal FALSE play)
-		(sunny    cool normal FALSE play)
-		(rainy    mild normal FALSE play)
-		(rainy    mild high   TRUE  skip)
-		(sunny    mild normal TRUE  play)
-		(overcast mild high   TRUE  play)
-		(overcast hot  normal FALSE play))))
-  
-  (defun random-test-nb1 (&optional (n 0.3) (str t))
-    (let* (train 
-	   test
-	   (k         (* n (length egs))))
-      (dolist (eg (shuffle egs))
-	(if (> (decf k) 0)
-	    (push eg test)
-	    (push eg train)))
-      (nb (make-weather train)
-	  (make-weather test)
-	  :stream str)))
-  
-  (defun self-test-nb ()
-    (nb (make-weather egs) 
-	(make-weather egs)))
- 
- )
+(defun hp-contains (tbl min-array max-array seen-array klass i value)
+  (if (numericp (header-name (nth i (table-columns tbl))))
+        (if (and
+             (>= (grabMinMaxValue tbl max-array klass i) value)
+             (<= (grabMinMaxValue tbl min-array klass i) value)
+             )1 0)
+        (if (= (grabSeenValue tbl seen-array klass i value) 1)
+            1
+            0)))
+
+(defun hp-mostContained (tbl instance min-array max-array seen-array)
+  (let ((best most-negative-fixnum)
+        (what "")
+        (count 0)
+        (classi (table-class tbl))
+        (all-attributes (eg-features instance))
+        (all-klasses (klasses tbl)))
+    (dolist (per-klass all-klasses what)
+      (setf count 0)
+      (doitems (per-attribute i all-attributes)
+              (unless (= classi i)
+                (unless (unknownp per-attribute)
+                  (setf count (+ count (hp-contains tbl min-array max-array seen-array per-klass i per-attribute))))))
+      (setf count (/ count (- (table-width tbl) 1)))
+      (cond ((>= count best)
+             (setf best count)
+             (setf what per-klass))))))
+      
+    
+;;Helpers functions follow!
+
+(defun buildMinMaxArr (tbl)
+  (make-array (list (length (klasses tbl)) (table-width tbl)) :initial-element 0))
+
+(defun setMinMaxValue (tbl arr klass i value)
+  (setf (aref arr (position klass (klasses tbl))
+              i)
+        value))
+
+(defun grabMinMaxValue (tbl arr klass i)
+  (aref arr (position klass (klasses tbl))
+        i))
+
+(defun buildSeenArr (tbl)
+  (let ((maxValue 0))
+    (dolist (per-column (table-columns tbl))
+      (if (typep per-column 'discrete)
+          (setf maxValue (max maxValue (length(discrete-uniques per-column))))))
+    (make-array (list (length (klasses tbl)) (table-width tbl) maxValue) :initial-element 0)))
+
+(defun setSeenArr (tbl arr klass i value)
+  (setf (aref arr (position klass (klasses tbl))
+              i
+              (position value
+                        (discrete-uniques
+                         (nth i (table-columns tbl)))))
+        1))
+
+(defun grabSeenValue (tbl arr klass i value)
+  (aref arr (position klass (klasses tbl)) i
+        (position value
+                  (discrete-uniques
+                         (nth i (table-columns tbl))))))
