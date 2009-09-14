506c13c7f6a4b68a48fb647d62e92047ad0487cf
mparsons6 <mparsons6@gmail.com>
2009-09-14 23:08:27 +0000

    Added 4 chapter 3 tests

--- our/etc/proj/1/chapter3.lisp
+++ our/etc/proj/1/chapter3.lisp
@@ -54,3 +54,56 @@
     (equal '(a b c) (reverse '(c b a)))
   )
 )
+
+; 3.6 (2 tests)
+(defun compress (x)
+  (if (consp x)
+      (compr (car x) 1 (cdr x))
+      x))
+
+(defun compr (elt n lst)
+  (if (null lst)
+      (list (n-elts elt n))
+      (let ((next (car lst)))
+	(if (eql next elt)
+	    (compr elt (+ n 1) (cdr lst))
+	    (cons (n-elts elt n)
+		  (ocmpr next 1 (cdr lst)))))))
+
+(defun n-elts (elt n)
+  (if (> n 1)
+      (list n elt)
+      elt))
+
+; Could also do: (equal '(tens) (n-elts 1 tens))
+(deftest test-n-elts ()
+  (check
+   (equal '(4 tens) (n-elts 4 tens))))
+
+(deftest test-compress ()
+  (check
+   (equal '(0 (4 1) 3 (2 2) 8 (3 4)) (compress '(0 1 1 1 1 3 2 2 8 4 4 4)))))
+
+; 3.7 (2 tests)
+(defun uncompress (lst)
+  (if (null lst)
+      nil
+      (let ((elt (car lst))
+	    (rest (uncompress (cdr lst))))
+	(if (consp elt)
+	    (append (apply #'list-of elt)
+		    rest)
+	    (cons elt rest)))))
+
+(defun list-of (n elt)
+  (if (zerop n)
+      nil
+      (cons elt (list-of (- n 1) elt))))
+
+(deftest test-list-of ()
+  (check
+   (equal '(ohthisisit ohthisisit ohthisisit) (list-of 3 'ohthisisit))))
+
+(deftest test-uncompress ()
+  (check
+   (equal '(0 1 1 1 1 2 3 6 6)  (uncompress '(0 (4 1) 2 3 (2 6))))))
