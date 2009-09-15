b2e743955c7cb5a7edd7c6711aae60b9b17fc321
explodeyourself <explodeyourself@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-15 03:41:24 +0000

    late stuff

--- our/etc/proj/1/chapter4.lisp
+++ our/etc/proj/1/chapter4.lisp
@@ -1,9 +1,38 @@
-;Test Count: 1
+;Test Count: 7
 
+;4.1 Arrays
 
+(let ((arr nil))
+  (deftest test-4-1 ()
+    (setf arr (make-array '(2 3) :initial-element nil))
+    (setf (aref arr 0 0) 'gus)
+    (check
+      (equalp (aref arr 0 0) 'gus))))
 
+;4.3 Sort function
+(let ((str "window"))
+  (deftest test-4-3 ()
+    (check
+      (string-equal (sort str #'char<) "dinoww"))))
 
+;4.4 Sequences
 
+(let ((str "please"))
+  (deftest test-4-4_1 ()
+    (check
+      (eql (position #\e str) 2)))
+  (deftest test-4-4_2 ()
+    (check
+      (eql (position #\e str :start 3) 5)))
+  (deftest test-4-4_3 ()
+    (check
+      (eql (find #\a str) #\a)))
+  (deftest test-4-4_4 ()
+    (check
+      (string-equal (remove-duplicates str) "plase"))))
+  
+
+  
 ; Figure 4.2
 ; Identifying tokens
 
@@ -27,7 +56,7 @@
 
 (defstruct (node (:print-function
                   (lambda (n s d)
-                    (format s "#,~A" (node-elt n)))))
+                    (format s "#<~A>" (node-elt n)))))
   elt (l nil) (r nil))
 
 (defun bst-insert (obj bst <)
@@ -59,5 +88,19 @@
 (defun bst-max (bst)
   (and bst
        (or (bst-max (node-r bst)) bst)))
+;(defun get-elt (bst)
+  
 
-; Figure 4.6
+(let ((nums nil))
+  (dolist (x '(8000 4565 8 345678 44 88123 1234))
+    (setf nums (bst-insert x nums #'<)))
+  (deftest test-fig-4-5_1 ()
+    (check
+      (eql (bst-find 124 nums #'<) nil)))
+  (deftest test-fig-4-5_2 ()
+    (check
+      (eql (node-elt(bst-min nums)) 1234)))
+  (deftest test-fig-4-5_3 ()
+    (check
+      (eql (bst-max nums) 1234))))
+                                        ; Figure 4.6
