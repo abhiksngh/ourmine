40e0103202ab6111edecb7137ef7961f801776eb
dremelofdeath <dremelofdeath@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-15 02:37:38 +0000

    adding make to the deftest file; hopefully this should only have to happen in project 1

--- our/etc/proj/1/deftest.lisp
+++ our/etc/proj/1/deftest.lisp
@@ -54,3 +54,18 @@
 	(funcall one)
 	(format t "; unknown test: [~a]~%" one)))
   (tests-report))
+
+(defun make1 (files)
+  (let ((n 0))
+    (dolist (file files)  
+      (format t ";;;; ~a.lisp~%"  file) 
+      (incf n)
+      (load file))
+    (format t ";;;; ~a files loaded~%" n)))
+
+(defun make (&optional (verbose nil))
+  (if verbose
+      (make1 *files*)
+      (handler-bind 
+	  ((style-warning #'muffle-warning))
+	(make1 *files*))))
