11ec95730bc455e3bdc8e8d58dbb5e17462fe30f
eliflesher <eliflesher@gmail.com>
2009-09-15 00:55:49 +0000

    Commiting test for 3.12 (46 Deftests) currently working on 4.2

--- etc/proj/1/chapter3.lisp
+++ etc/proj/1/chapter3.lisp
@@ -138,3 +138,30 @@
 
 ;;; 3.12 Breadth-First Search [Required]
 
+(defun new-paths (path node net)
+  (mapcar #'(lambda (n)
+              (cons n path))
+          (cdr (assoc node net))))
+
+(defun bfs (end queue net)
+  (if (null queue)
+      nil
+      (let ((path (car queue)))
+         (let ((node (car path)))
+            (if (eql node end)
+                (reverse path)
+                (bfs end
+                     (append (cdr queue)
+                             (new-paths path node net))
+                     net))))))
+
+(defun shortest-path (start end net)
+  (bfs end (list (list start)) net ))
+
+(defun test-bfs ()
+  (let ((min '((a b c) (b c) (c d))))
+    (shortest-path 'a 'd min)))
+
+(deftest provebfs ()
+  (let ((min '((a b c) (b c) (c d))))
+    (check (equalp (shortest-path 'a 'd min) '(A C D)))))
