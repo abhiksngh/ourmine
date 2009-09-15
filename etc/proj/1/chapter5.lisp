c0bb7dbd22f0cec6b712a0d87564b7a12416a90d
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-15 01:55:12 +0000

    Added figure 5.2, deftests stand at 48.

--- etc/proj/1/chapter5.lisp
+++ etc/proj/1/chapter5.lisp
@@ -22,6 +22,65 @@
 
 ;;; Figure 5.2 Date Arithmetic [Required]
 
+(defconstant month #(0 31 59 90 120 151 181 212 243 273 304 334 365))
+(defconstant yzero 2000)
+
+(defun leap? (y)
+   (and (zerop (mod y 4))
+        (or (zerop (mod y 400))
+            (not (zerop (mod y 100))))))
+
+(defun nmon (n)
+   (let ((m (position n month :test #'<)))
+      (values m (+ 1 (- n (svref month (- m 1)))))))
+
+(defun year-days (y) (if (leap? y) 366 365))
+
+(defun num-year (n)
+  (if (< n 0)
+    (do* ((y (- yzero 1) (- y 1))
+          (d (- (year-days y)) (- d (year-days y))))
+         ((<= d n) (values y (- n d))))
+    (do* ((y yzero (+ y 1))
+          (prev 0 d)
+          (d (year-days y) (+ d (year-days y))))
+         ((> d n) (values y (- n prev))))))
+
+(defun year-num (y)
+   (let ((d 0))
+      (if (>= y yzero)
+          (dotimes (i (- y yzero) d)
+            (incf d (year-days (+ yzero i))))
+          (dotimes (i (- yzero y) (- d))
+            (incf d (year-days (+ y i)))))))
+
+(defun month-num (m y)
+    (+ (svref month (- m 1))
+       (if (and (> m 2) (leap? y)) 1 0)))
+
+(defun num-month (n y)
+   (if (leap? y)
+       (cond ((= n 59) (values 2 29))
+             ((> n 59) (nmon (- n 1)))
+             (t        (nmon n)))
+       (nmon n)))
+
+(defun num->date (n)
+  (multiple-value-bind (y left) (num-year n)
+     (multiple-value-bind (m d) (num-month left y)
+      (values d m y))))
+
+(defun date->num (d m y)
+    (+ (- d 1)  (month-num m y) (year-num y)))
+
+(defun date+ (d m y n)
+   (num->date (+ (date->num d m y) n)))
+
+(deftest inttodate () 
+  (check (equalp 
+    '(19 5 2012) 
+    (multiple-value-bind (x y z) (date+ 1 1 2009 1234) (list x y z)))))
+
 ;;; 5.3 Conditionals
 
 (deftest conditionals ()
