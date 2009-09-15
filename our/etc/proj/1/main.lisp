0858d32ac576031ab5103f5eac9a97421006fee0
dremelofdeath <dremelofdeath@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-15 02:41:23 +0000

    adding a few things to the main file. removing make from the deftest file. changing the string of loads into a defparameter *files* so that make will handle it.

--- our/etc/proj/1/main.lisp
+++ our/etc/proj/1/main.lisp
@@ -1,8 +1,23 @@
-(load "deftest.lisp")
-(load "chapter2")
-(load "chapter3")
-(load "chapter4")
-(load "chapter6")
-(load "chapter10")
-(print "Tests Ready")
+(defparameter *files* '("deftest.lisp"
+                        "chapter2"
+                        "chapter3"
+                        "chapter4"
+                        "chapter6"
+                        "chapter10"))
 
+(format t "Tests Ready")
+
+(defun make1 (files)
+  (let ((n 0))
+    (dolist (file files)  
+      (format t ";;;; ~a.lisp~%"  file) 
+      (incf n)
+      (load file))
+    (format t ";;;; ~a files loaded~%" n)))
+
+(defun make (&optional (verbose nil))
+  (if verbose
+      (make1 *files*)
+      (handler-bind 
+	  ((style-warning #'muffle-warning))
+	(make1 *files*))))
