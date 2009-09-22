439ab61b986b5e3222b67f6c5bd5ff35c1d73b0a
RabiTuckman <RabiTuckman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-22 19:23:04 +0000

    changed some flags

--- our/lib/sh/learn.sh
+++ our/lib/sh/learn.sh
@@ -1,6 +1,6 @@
 bnet(){
         local learner=weka.classifiers.bayes.BayesNet
-	$Weka $learner -t $1 -T $2 -D \
+	$Weka $learner -p 0 -t $1 -T $2 -D \
 	    -Q weka.classifiers.bayes.net.search.local.K2 -- -P 2 -S BAYES \
 	    -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 0.5 
 }
@@ -12,7 +12,7 @@ bnet10(){
 }
 nb() {
  	local learner=weka.classifiers.bayes.NaiveBayes
-	$Weka $learner -t $1 -T $2  
+	$Weka $learner -p 0 -t $1 -T $2  
 }
 nb10() {
 	local learner=weka.classifiers.bayes.NaiveBayes
@@ -20,7 +20,7 @@ nb10() {
 }
 j48() {
 	local learner=weka.classifiers.trees.J48
-	$Weka $learner -C 0.25 -M 2 -t $1 -T $2
+	$Weka $learner -p 0 -C 0.25 -M 2 -t $1 -T $2
 }
 j4810() {
 	local learner=weka.classifiers.trees.J48
@@ -28,7 +28,7 @@ j4810() {
 }
 zeror() {
         local learner=weka.classifiers.rules.ZeroR
-	$Weka $learner -t $1 -T $2
+	$Weka $learner -p 0 -t $1 -T $2
 }
 zeror10() {
         local learner=weka.classifiers.rules.ZeroR
@@ -36,7 +36,7 @@ zeror10() {
 }
 oner() {
         local learner=weka.classifiers.rules.OneR
-	$Weka $learner -t $1 -T $2
+	$Weka $learner -p 0 -t $1 -T $2
 }
 oner10() {
         local learner=weka.classifiers.rules.OneR
@@ -44,7 +44,7 @@ oner10() {
 }
 rbfnet(){
         local learner=weka.classifiers.functions.RBFNetwork
-	$Weka $learner -t $1 -T $2 -B 2 -S 1 -R 1.0E-8 -M -1 -W 0.1
+	$Weka $learner -p 0 -t $1 -T $2 -B 2 -S 1 -R 1.0E-8 -M -1 -W 0.1
 }
 rbfnet10(){
         local learner=weka.classifiers.functions.RBFNetwork
@@ -52,7 +52,7 @@ rbfnet10(){
 }
 ridor() {
        local learner=weka.classifiers.rules.Ridor
-	$Weka $learner -F 3 -S 1 -N 2.0 -t $1 -T $2 
+	$Weka $learner -F 3 -S 1 -N 2.0 -p 0 -t $1 -T $2 
 }
 ridor10(){
        local learner=weka.classifiers.rules.Ridor
@@ -60,11 +60,11 @@ ridor10(){
 }
 adtree() {
        local learner=weka.classifiers.trees.ADTree
-       $Weka $learner -B 10 -E -3 -t $1 -T $2
+       $Weka $learner -B 10 -E -3 -p 0 -t $1 -T $2
 }
 adtree10() {
        local learner=weka.classifiers.trees.ADTree
-       $Weka $learner -B 10 -E -3 -i -t $1
+       $Weka $learner -B 10 -E -3 -p 0 -i -t $1
 }
 
 
