5119d716b651a59bbb429d6da99c2973768f92d0
tomi.prifti <tomi.prifti@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-09-27 05:02:57 +0000

    k-m

--- our/lib/lisp/proj2/k-means.lisp
+++ our/lib/lisp/proj2/k-means.lisp
@@ -3,23 +3,52 @@
          (size (length instances))
          (listK '())
          (lstCentroids '())
+         (oldCentroids '())
          (clusters '()))
     (dotimes (i k)
       (setf listK (cons (get-rand-k size listK) listK)))
     (setf lstCentroids (get-centroids listK instances))
+    (setf oldCentroids (copy-tree lstCentroids))
     (setf clusters (make-clusters lstCentroids))
-    (dolist (inst instances clusters)
-      (let* ((cls (calc-dist inst lstCentroids)))
-        (setf (nth cls clusters) (cons inst (nth cls clusters)))))))
+    (setf clusters (create-new-clusters instances lstCentroids clusters))
+    (setf lstCentroids (new-centroids clusters))
+    (do ()
+       ( (check-all-centroids oldCentroids lstCentroids) clusters)
+      (setf clusters (create-new-clusters instances lstCentroids (make-clusters lstCentroids)))
+      (setf oldCentroids (copy-tree lstCentroids))
+      ;(format t "Old Centorids ~A~%" oldCentroids)
+      (setf lstCentroids (new-centroids clusters))
+      ;(format t "New Centorids ~A~%" lstCentroids))))
+      
+    
+
+
+    
+
+;;move instances into clusters
+(defun create-new-clusters (instances centroids clusters)
+  (dolist (inst instances clusters)
+    (let* ((cls (calc-dist inst centroids)))
+      (setf (nth cls clusters) (cons inst (nth cls clusters))))))
+
+
+;; determine all the centroids have moved
+(defun check-all-centroids (oldcents newcents)
+  (if (or (null oldcents)
+          (null newcents))
+      t
+      (if (not (centroids-move (car oldcents) (car newcents)))
+          (return-from check-all-centroids nil)
+          (check-all-centroids (cdr oldcents) (cdr newCents)))))
 
 ;; determine if the centroids have moved
 (defun centroids-move (oldcents newcents &optional (indx 0))
-     (if (not (equal (nth indx oldcents)
-                        (nth indx newcents)))
-             (return-from centroids-move nil)
-                 (if (= indx (length oldcents))
-                     (return-from centroids-move t)
-                     (centroids-move oldcents newcents (incf indx)))))
+  (if (not (equal (nth indx oldcents)
+                  (nth indx newcents)))
+      (return-from centroids-move nil)
+      (if (= indx (length oldcents))
+          (return-from centroids-move t)
+          (centroids-move oldcents newcents (incf indx)))))
 
 ;;get random position from the total size
 (defun get-rand-k (size lst)
@@ -56,3 +85,54 @@
      (dolist (obj sum)
        (setf (nth (position obj sum) sum)(float (/ obj len))))
      sum))
+
+;;define the new centroids
+(defun new-centroids (lstClusters)
+  (let* ((lst '()))
+    (dolist (cls lstClusters lst)
+      (setf lst (append lst (list (cluster-mean cls)))))))
+
+
+(defun make-data-k ()
+  (data
+   :name   'weather
+   :columns '($att1 $att2 $att3 $att4)
+   :egs    '((10    5  45   20 ) 
+	     (3    56  4   10 ) 
+	     (4    4  4   30  ) 
+             (5    5  5   20.2 )
+             (10    6  6   20.1 )
+             (34    67  64   20.7)
+             (10    5  45   20 ) 
+	     (3    56  4   10 ) 
+	     (4    4  4   30  ) 
+             (5    5  5   20.2 )
+             (10    6  6   20.1 )
+             (34    67  64   20.7)
+             (10    5  45   20 ) 
+	     (6    56  4   10 ) 
+	     (8    4  4   30  ) 
+             (9    5  5   20.2 )
+             (7    6  6   20.1 )
+             (6    67  64   20.7)
+             (9    5  45   20 ) 
+	     (23    56  4   10 ) 
+	     (6    4  4   30  ) 
+             (8    5  5   20.2 )
+             (9    6  6   20.1 )
+             (8    67  64   20.7)
+             (10    5  4   20 ) 
+	     (3    56  7   10 ) 
+	     (4    4  67   30  ) 
+             (5    5  67   20.2 )
+             (10    6  67   20.1 )
+             (34    67  64   7.7)
+             (10    5  45   5 ) 
+	     (3    56  4   78 ) 
+	     (4    4  4   67  ) 
+             (5    5  5   45.2 )
+             (10    6  6   67.1 )
+             (34    67  64   67.7)
+             )))
+
+
