e117d096445874ac3e3653bdfbc891ac5b22732f
w.w.mensah <w.w.mensah@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-01 02:32:07 +0000

    metrics

--- our/lib/lisp/proj2/metrics.lisp
+++ our/lib/lisp/proj2/metrics.lisp
@@ -1,22 +1,3 @@
-(defun metrics (A B C D args &optional header)
-  "args is a list of the functions that were passed to the learner"
-  (let* ((prec (/ D (+ C D)))
-         (acc (/ (+ A D) (+ A B C D)))
-         (pd (/ D (+ B D)))
-         (pf (/ C (+ A C)))
-         (f (/ (* 2 prec 1) (+ prec 1))) ; 1 = recall, whatever recall is
-         (g (/ (* 2 pf pd) (+ pf pd)))
-         (params (list A B C D acc prec pd pf f g)))
-    (setf args (check-for-nulls args))
-    (setf table (list (append args params)))
-    (if (null header)
-        (setf table (cons (get-header) table)))
-    (format t "~:{~&~8@A, ~8@A, ~8@A,  ~8@A,   ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A ~}" table)) t)
-
-(defun display-header ()
-  (format nil "~:{~D,~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A ~}"
-          '((prep discretizer cluster fss classify class a b c d acc prec pd pf f g))))
-
 (defun check-for-nulls (args)
   "checks for null arguments in 'args' and replaces them with 'doNothing"
   (dolist (item args args)
@@ -26,3 +7,34 @@
 (defun get-header()
   (setf header '(prep discretizer cluster fss classify class a b c d acc prec pd pf f g))
   header)
+
+
+(defun compute-metrics (A B C D)
+  (let* ((prec (/ D (+ C D)))
+         (acc (/ (+ A D) (+ A B C D)))
+         (pd (/ D (+ B D)))
+         (pf (/ C (+ A C)))
+         (f (/ (* 2 prec (/ pd pf)) (+ prec (/ pd pf))))
+         (g (/ (* 2 pf pd) (+ pf pd)))
+         (params (list A B C D acc prec pd pf f g)))
+    params))
+
+
+(defun row-metrics (params A B C D)
+  "creates a simgle row that would be added to a table of metrics to be printed out
+params is the parameters passed to 'learn' as a list"
+  (let* ((table))
+    (setf params (check-for-nulls params))
+    (setf table (list (append (compute-metrics A B C D) params)))
+    table))
+
+
+(defun print-metrics (table)
+  "takes a table containing rows of data to be printed out, appends a header to it and prints it out"
+  (setf table (cons (get-header) table))
+  (format t "~:{~&~8@A, ~8@A, ~8@A,  ~8@A,   ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A, ~8@A ~}" table))
+
+
+;; (setf row (row-metrics '(param1, param2, param3...) 1 2 3 4))
+;; repeat for any other row of data, until you have a list of lists representing the table to be printed then call
+;; (print-metrics table)
