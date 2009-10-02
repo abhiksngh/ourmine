238169acf43a1ba0766bb63be76adb992fa72c7a
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-02 22:03:44 +0000

    Adding preliminary files for subsample pre-processor, with algorithm comments.

---
+++ etc/proj/2/subsample.lisp
@@ -0,0 +1,11 @@
+;;; Subsample: Poll data for a tally on all the classes.  Find the smallest class, and randomly delete
+;;;   lines until they are all equal in prevelence.  Once the classes are all the same size, the data
+;;;   is now ready for the discretizer.
+
+(defun subsample (data)
+   ;;; Poll for frequency of all class types.  Keep tally.
+   ;;; Identify smallest class, hold this value.
+   ;;; Begin to do multiple passes over the data.  If a class is encountered that is too large,
+   ;;;   randomly decide to remove it or not, decrement tally.
+   ;;; At the end of a pass, if all counts are not equal, pass again.  Else, return data subset.
+)
