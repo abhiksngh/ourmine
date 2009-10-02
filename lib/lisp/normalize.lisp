b2bb07392f26f3150749f5c8bd89c286fc84ac24
canada11278 <canada11278@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-02 21:40:51 +0000

    Adding more actual code to normalize

--- lib/lisp/normalize.lisp
+++ lib/lisp/normalize.lisp
@@ -2,7 +2,7 @@
   (data
    :name 'test
    :columns '($number1 symbol $number2 stuff)
-   :egs '((1 hey 2 true)
+   :egs '((2 hey 2 true)
           (2 hey 2 true)
           (3 hey 2 true)
           (4 hey 2 false)
@@ -10,15 +10,18 @@
 )))
 
 (defun normalize ()
-  (let ((tbl (xindex (test-data))))
-    (dolist (col (table-columns tbl))
-      (if (numericp (header-name col)) ;checks if the current column is numeric
-          (if (/= 0 ( )) ;checks if max-min is not 0
-              (progn
-                (dolist (row (tbl-f))
-                  (if (numberp ) ;check if entry in the column and row is a number
-                      ()))) ;setf data (/(- row min) (- max min))
-             (progn
-               (dolist (row (tbl-f))
-                 (if (numberp ) ;check if entry in the colum and row is a number
-                     ())))))))) ;setf data 1.0
+  (let* ((tbl (xindex (test-data)))
+         (n 0))
+    (dolist (col (table-columns tbl) tbl)
+      (if (numericp (header-name col))
+          (progn
+            (if (zerop (max-min)) ;checks if max-min is zero to avoid dividing by 0
+                (progn
+                  (dolist (row (table-all tbl)) ;loop through each row of data
+                    (if (numberp (nth n (eg-features row))) ;checks if that data is a number
+                        (setf (nth n (eg-features row)) 1.0)))) ;sets that data to 1.0 instead of dividing by 0
+                (progn
+                  (dolist (row (table-all tbl)) ;loop through each row of data
+                    (if (numberp (nth n (eg-features row))) ;checks if the data is a number
+                        (setf (nth n (eg-features row)) (/ (- (nth n (eg-features row)) min) (- max min))))))))) ;normalizes the number
+      (setf n (+ n 1))))) ;increment the counter of the row being altered
