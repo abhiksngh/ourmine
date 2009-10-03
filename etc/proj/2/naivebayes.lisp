067e52e4adf90dc9639cd682de5fd736e71feda2
eliflesher <eliflesher@gmail.com>
2009-10-03 00:16:17 +0000

    Adding comments and outline for naive bays

--- etc/proj/2/naivebayes.lisp
+++ etc/proj/2/naivebayes.lisp
@@ -1,1 +1,3 @@
-;;; Claimee: Elijah
+;; Naive bays basically breaks the data set into groups basd on each column and the outcome. So, for each column, it breaks the data into classes and then finds the class ratios - so determines how often a particular value for a class occurs for each outcome... multiply the ratios and then output which is more likely.
+
+;; Claimee: Elijah
