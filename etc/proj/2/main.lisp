2ee8445ee7df418a4c6dca5fd6afe5d20f262dc5
eliflesher <eliflesher@gmail.com>
2009-10-03 02:18:23 +0000

    Initial skeleton for learn

--- etc/proj/2/main.lisp
+++ etc/proj/2/main.lisp
@@ -1,7 +1,39 @@
-(load binlogging.lisp)
-(load genic.lisp)
-(load main.lisp)
-(load naivebayes.lisp)
-(load relief.lisp)
+;; Main.lisp
+;; This file loads the basic function for each of our data mining techniques
+;; and defuns the learn function - which executes them based on function passing
+
+(print "Loading Pre-processors...")
 (load subsample.lisp)
+(print "Complete.")
+
+(print "Loading Discretizers...")
+(load binlogging.lisp)
+(print "Complete.")
+
+(print "Loading Clusterers...")
+(load genic.lisp)
+(print "Complete.")
+
+(print "Loading Feature Subset Selectors..."
+(load relief.lisp)
+(print "Complete.")
+
+(print "Loading Classifiers...")
+(load naivebayes.lisp)
 (load twor.lisp)
+(print "Complete.")
+
+(defun learn (&key 	(k 	10)
+			(preprocessor	#'subsample)
+			(discretizer	#'binlog)
+			(clusterer	#'genic)
+			(fss		#'relief)
+			(classifier	#'naivebayes)
+			(train		"train.lisp")
+			(test		"test.lisp"))
+	(let ((training (load train)))
+	     ((testing (load test)))
+	
+	
+	)
+)
