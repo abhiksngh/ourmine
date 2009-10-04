259507060cfcdb18feb328afb164f030eef25721
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-04 00:02:16 +0000

    Adding utility functions for subsample.

--- etc/proj/2/algos/subsample.lisp
+++ etc/proj/2/algos/subsample.lisp
@@ -6,9 +6,35 @@
 
 (print " - Loading Subsample") ;;; Logging for a pretty run.
 
-(defun subsample (data)
-  (let ((minorityClass nil) (classTally nil))
-     ;;; Poll for frequency of all class types.  Keep tally.
+(defun count-classes (table)
+  (let ((class-count (make-hash-table)) (seen nil) (ranks nil))
+    (dolist (x (table-all table)) 
+      (if (null (gethash (eg-class x) class-count))
+        (setf (gethash (eg-class x) class-count) 1 seen (append seen (list (eg-class x))))
+        (setf (gethash (eg-class x) class-count) (+ 1 (gethash (eg-class x) class-count)))
+      )
+    )
+    (dolist (x seen)
+      (setf ranks (append ranks (list (list x (gethash x class-count)))))
+    )
+    ranks
+  )
+)
+
+(defun minority-class (table)
+  (let ((minority (list nil 0)) (ranks (count-classes table)))
+    (dolist (x ranks)
+      (if (or (< (second x) (second minority)) (equalp (second minority) 0))
+        (setf minority x)
+      )
+    )
+    (first minority)
+  )
+)
+
+(defun subsample (table)
+  (let ((minority (minority-class table)) (ranks (count-classes table)))
+   ;;; Poll for frequency of all class types.  Keep tally.
    ;;; Identify smallest class, hold this value.
    ;;; Begin to do multiple passes over the data.  If a class is encountered that is too large,
    ;;;   randomly decide to remove it or not, decrement tally.
