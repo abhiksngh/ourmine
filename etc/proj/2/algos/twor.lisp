4efa9bd9736a0eef42893dcb81e1c2dcebb5c2a4
timbielawa <timbielawa@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-04 09:33:52 +0000

    got the struct generator, the struct table generator, and a prototype for the main method ready to go.

--- etc/proj/2/algos/twor.lisp
+++ etc/proj/2/algos/twor.lisp
@@ -3,12 +3,38 @@
 
 (print " - Loading TwoR") ;; Output for a pretty log
 
-
-(defun mkstr (tbl)
+(defun mkstruct (tbl)
   "Makes a structure based off of the unique classes in a dataset"
-  (let ((l (justclasses tbl)))
-    (progn
-      (push 'classstr l)
-      (push 'defstruct l))
-    (eval l)))
+; Use as: (make-class-str
+  (let ((x (justclasses tbl)) (m '()))
+    (dotimes (n (length x))
+      (push `(,(pop x) 0) m))
+    (push 'class-str m)
+    (push 'defstruct m)
+    (eval m)))
 
+(defun make-stat-table (tbl)
+  "Creates an assoc list of structs designed to hold frequency data structs"
+  (let ((strlst '()) (col 0))
+    (dolist (l (allvals tbl))
+      (let ((values '()))
+	(dotimes (n (length l))
+	  (push (cons (nth n l) (make-class-str)) values))
+	(push (cons col values) strlst))
+      (incf col))
+    (cdr strlst)))
+
+(defun twor (tbl)
+  (let* ((stat-struct (mkstruct tbl))
+	 (stat-table (make-stat-table(tbl))))
+    (dolist (l (allvals tbl))
+      (print l)))
+  tbl)
+
+   
+
+
+
+;;for each column a
+;;;;;;for each value, v, in a
+;;;;;;;;;;
