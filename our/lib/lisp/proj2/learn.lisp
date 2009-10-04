37a07be6a5a299d90294d10d761fbbf95a1f5bd2
tomi.prifti <tomi.prifti@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-04 06:32:21 +0000

    learnFun

--- our/lib/lisp/proj2/learn.lisp
+++ our/lib/lisp/proj2/learn.lisp
@@ -1,14 +1,23 @@
 (defun learn (&key (k 8)
-                   (cluster   #'(lambda (data) (k-means data k))))
+              (cluster   #'(lambda (data) (k-means data k))))
 
   (let* ((train (make-data-k))
          (clusters (funcall cluster train))
-         (cluster-tables '())
+         (cluster-tables '()))
+    
+    (setf cluster-tables (make-cluster-tables clusters train))))
+
+
+
+
+(defun make-cluster-tables (clusters train)
+  (let* ((cluster-tables '())
          (cluster-nr 1))
-    (dolist (obj clusters cluster-tables)
-      (setf cluster-tables (cons cluster-tables
-                                 (data :name cluster-nr :columns (get-col-names (table-columns train)) :egs obj)))
-      (setf cluster-nr (incf cluster-nr)))))
+    (dolist (obj clusters)
+      (setf cluster-tables (append cluster-tables
+                                   (list (data :name cluster-nr :columns (get-col-names (table-columns train)) :egs obj))))
+      (setf cluster-nr (incf cluster-nr)))
+    (del-empty-clusters cluster-tables)))
 
 
 
@@ -16,4 +25,16 @@
   (let* ((lst-cols))
   (dolist (obj lst (reverse lst-cols))
     (setf lst-cols (cons (header-name obj) lst-cols)))))
-              
+
+
+;(defun get-cls-means(cluster-tables)
+;  (let* ((centroids '()))
+ ;   (dolist (obj cluster-tables centroids)
+  ;    (let* ((features (get-features (table-all obj)))
+   ;          (class (table-class obj)))
+    ;    (setf centroids (append (cluster-mean features class) centroids))))))
+
+(defun del-empty-clusters (cluster-tables)
+  (dolist (obj cluster-tables cluster-tables)
+    (if (not (table-all obj))
+        (setf cluster-tables (remove obj cluster-tables)))))
