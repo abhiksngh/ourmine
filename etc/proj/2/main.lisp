63a305409547b5ea20d77b3387fb9982a2638941
eliflesher <eliflesher@gmail.com>
2009-10-04 17:33:46 +0000

    Updating main.lisp to load utility function and library

--- etc/proj/2/main.lisp
+++ etc/proj/2/main.lisp
@@ -2,11 +2,17 @@
 ;; This file loads the basic function for each of our data mining techniques
 ;; and defuns the learn function - which executes them based on function passing
 
+(print "Loading Utility Libraries...")
+(load "lib/loaddeps")
+(load "utils/utils")
+(print "Complete.")
+
 (print "Loading Pre-processors...")
 (load "algos/subsample")
 (print "Complete.")
 
 (print "Loading Discretizers...")
+(load "algos/nbins")
 (load "algos/binlogging")
 (print "Complete.")
 
