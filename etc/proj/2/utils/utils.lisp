036119c830d5e0b38d7fd241cd828033b8da326c
eliflesher <eliflesher@gmail.com>
2009-10-04 02:44:29 +0000

    Correcting utils

--- etc/proj/2/utils/utils.lisp
+++ etc/proj/2/utils/utils.lisp
@@ -2,6 +2,10 @@
 	(dolist (item (table-all tbl)) 
 		(setf (eg-features item) (mapcar func (eg-features item)))))
 
+(defun do-over-specific-feature (tbl func column)
+        (dolist (item (table-all tbl))
+                (setf (nth column (eg-features item))  (funcall func (nth column (eg-features item))))))
+
 (defun gimme-classes (tbl)
 	(mapcar #'eg-class (table-all tbl)))
 
@@ -20,28 +24,40 @@
   )
 )
 
-(defun minority-class (table)
-  (let ((minority (list nil 0)) (ranks (count-classes table)))
-    (dolist (x ranks)
-      (if (or (< (second x) (second minority)) (equalp (second minority) 0))
-        (setf minority x)
+(defun count-uniques (table opener)
+  (let ((item-count (make-hash-table)) (seen nil) (ranks nil))
+    (dolist (x (table-all table))
+      (if (null (gethash (funcall opener x) item-count))
+        (setf (gethash (funcall opener x) item-count) 1 seen (append seen (list (funcall opener x))))
+        (setf (gethash (funcall opener x) item-count) (+ 1 (gethash (funcall opener x) item-count)))
       )
     )
-    (first minority)
-  )
-)
-
-(defun minority-diff (table)
-  (let ((ranks (count-classes table)) (minority (minority-class table)) (minimum nil))
-    (dolist (x ranks)
-      (if (equalp minority (first x))
-        (setf minimum (second x) ranks (remove x ranks))
-      )
-    )
-    (dolist (x ranks)
-      (setf (second x) (- (second x) minimum))
+    (dolist (x seen)
+      (setf ranks (append ranks (list (list x (gethash x item-count)))))
     )
     ranks
   )
 )
 
+(defun list-unique-features (table)
+  (let ((uniques nil))
+    (dotimes (doer (length (table-columns table)) (reverse uniques))
+      (push (count-uniques table #'(lambda (x) (nth doer (eg-features x)))) uniques ))))
+
+(defun count-unique-features (table)
+  (mapcar #'length (list-unique-features table)))
+
+(defun find-testiest (l test)
+  (let ((testiest (car l)))
+    (dolist (item l testiest)
+      (setf testiest (funcall test testiest item)))))
+
+(defun find-testiest-numerics (table test)
+  (let ((testiest-numerics nil))
+    (let ((uniques (list-unique-features table)))
+      (dotimes (doer (length (table-columns table)) (reverse testiest-numerics))
+        (if (numeric-p (nth doer (table-columns table)))
+	  (push (find-testiest (mapcar #'car (nth doer uniques)) test) testiest-numerics)
+          (push nil testiest-numerics))))))
+      
+      
