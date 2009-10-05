1f0cffe7e3816873f6f4ff45f21049c61098fcd9
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-05 20:52:52 +0000

    Adding diff-metric finder function for RELIEF.

--- etc/proj/2/algos/relief.lisp
+++ etc/proj/2/algos/relief.lisp
@@ -4,6 +4,8 @@
 
 (print " - Loading Relief") ;; Output for a pretty log
 
+;;; Find closest neighbor.  Find-same-class is for locating closest hit and closest miss.
+
 (defun closest-member (table row-number find-same-class)
   (let ((target-class (eg-class (nth row-number (table-all table)))))
     (loop for i from 1 to (length (table-all table)) do
@@ -30,5 +32,17 @@
 )
 
 ;;; Find difference metric.
+
+(defun relief-diff (column-num target neighbor &optional (divisor-n 1))
+  (let ((score 0) (target-feature (nth column-num (eg-features target))) (neighbor-feature (nth column-num (eg-features neighbor))))
+    (if (and (realp target-feature) (realp neighbor-feature))
+      (setf score (+ score (/ (abs (- target-feature neighbor-feature)) (+ target-feature neighbor-feature))))
+      (if (equalp target-feature neighbor-feature)
+        (setf score (+ score 1))
+      )
+    )
+    (/ score divisor-n)
+  )
+)
 
 ;;; Run loop n times, return list of weights.
