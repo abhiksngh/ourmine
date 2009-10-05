2854a3e0495b7a8002db43def27aa1e8154b9da6
timbielawa <timbielawa@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-05 02:51:53 +0000

    Most of the oneR part works now. Need to sort through the list structure it returns to start making the rules.

--- etc/proj/2/algos/twor.lisp
+++ etc/proj/2/algos/twor.lisp
@@ -3,8 +3,6 @@
 
 (print " - Loading TwoR") ;; Output for a pretty log
 
-(load "lib/loaddeps")
-(load "utils/utils")
 (load "d-data/weathernumerics.lisp")
 
 (defparameter w (weather-numerics))
@@ -25,62 +23,60 @@
 (defun majority-class-list (tbl)
   "Return a list containing features of instances in the majority class"
   (mapcar 'eg-features ((lambda (x) (let ((y '())) (dolist (l (egs x) y) (if (eql (majority-class x) (eg-class l)) (push l y))))) tbl)))
-	
  
- (defun oner(tbl)
+(defun oner(tbl)
   (let* 
       ((cols (columns-header (table-columns tbl)))
-        (major-features (majority-class-list tbl))
+       (major-features (majority-class-list tbl))
        (counts-all '())
-       (i 0))
+       (i 0)
+       (accuracy '()))
     (dolist (col (reverse (cdr (reverse cols)))) ;for each column
       (let ((counts '()))
 	(dolist (l major-features) ; for each instance
 	  (let ((this (nth i l)))
 	    (if (null counts) ; is it a new list? then put in first value w/ '1'
 		(push (cons this 1) counts)
-	      (if (null (assoc this counts)) ;this isn't in the list yet
-		  (push (cons this 1) counts) ; add it
-		  (incf (cdr (assoc this counts)))))))
+		(if (null (assoc this counts)) ;this isn't in the list yet
+		    (push (cons this 1) counts) ; add it
+		    (incf (cdr (assoc this counts)))))))
 	(incf i)
 	(push (cons col counts) counts-all)))
     (reverse counts-all)
-
-    ;We use accuracy to store the counts for each column
-    (let ((accuracy '())
-
-	  ; j allows us to iterate over list in 'feats'
-	  (j 0)
+    (let 
+	((j 0) ; j allows us to iterate over list in 'feats'  
 	  ; feats represents all observed features.
 	  ; feats contains a list for each column (attribute)
 	  ; each list stores an assoc list of the attr values and
 	  ; how many times each one appeared.
-	  (feats (list-unique-features tbl))
-	  (cols (columns-header (table-columns tbl))))
+	 (feats (list-unique-features tbl))
+	 (cols (columns-header (table-columns tbl))))
       ; col is the column we are classifying 
       (dolist (col (reverse (cdr (reverse cols)))) ; drop the classes column, put back in order...
 	; the jist of this method is to iterate over each column of attributes
 	; Find out how many times an attribute value appeared over all
 	; Find out how many tmes an attribute value appeared in the target class
 	; This dolist will loop for each column
-
 	(let*
-	    ; select the present working column, j, from the full set
-	    ((these-tokens-tuples (nth j feats))
+	; select the present working column, j, from the full set
+	    ((subaccuracy '())
+	     (these-tokens-tuples (nth j feats))
 	     (these-tokens (mapcar 'car these-tokens-tuples)) ; list of observed attr values in this column
 	     (class-count-list (cdr (assoc col counts-all)))) ; list of the counts of class intances in this column
-
 	  ;(print these-tokens-tuples)    ; like this: ((RAINY 5) (OVERCAST 4) (SUNNY 5))
 	  ;(print (cdr (assoc col counts-all))) ; like this: ((SUNNY . 2) (RAINY . 3) (OVERCAST . 4))
-
-	  (dolist
-	      (this-token these-tokens)
-	    (let* 
+	  (dolist (this-token these-tokens)
+	    (let*
 		((full-count (nth 1 (assoc this-token these-tokens-tuples)))
 		 (class-count (cdr (assoc this-token class-count-list)))
-		 (measure (/ class-count full-count)))
-	      (format t "Occurrences of: ~A~% Total: ~A;    Class: ~A~%" this-token full-count class-count))))
-	    
-	(incf j)))))
-      
-
+		 (rating nil))
+	      (if
+	       (numberp class-count)
+	       (progn
+		 ;(format t "Processing: ~A, ~A/~A~%" this-token class-count full-count)
+		 (setf rating (cons this-token (/ class-count full-count)))
+		 (push rating subaccuracy)))))
+	  ;(format t "Skipping: ~A, ~A/~A~%" this-token class-count full-count))))
+	  (push subaccuracy accuracy))
+	(incf j)))
+    accuracy))
