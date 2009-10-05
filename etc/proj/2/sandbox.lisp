466b87fce88e419548028d4349cafe6664059a84
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-05 20:36:16 +0000

    Adding nearest same-class/diff-class function for RELIEF.

--- etc/proj/2/sandbox.lisp
+++ etc/proj/2/sandbox.lisp
@@ -8,6 +8,25 @@
 
 (defun closest-member (table row-number find-same-class)
   (let ((target-class (eg-class (nth row-number (table-all table)))))
-    
+    (loop for i from 1 to (length (table-all table)) do
+      (let ((left (nth (- row-number i) (table-all table))) (right (nth (+ row-number i) (table-all table))))
+        (if (equalp (eg-class left) target-class)
+          (if find-same-class
+            (return (- row-number i))
+          )
+          (if (not find-same-class)
+            (return (- row-number i))
+          )
+        )
+        (if (equalp (eg-class right) target-class)
+          (if find-same-class
+            (return (+ row-number i))
+          )
+          (if (not find-same-class)
+            (return (+ row-number i))
+          )
+        )
+      )
+    )
   )
 )
