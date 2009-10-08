d86f6bbb339a672f689ea915c269438be4e6ff15
canada11278 <canada11278@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-08 03:17:35 +0000

    Fixed Kmeans

--- lib/lisp/kmeans.lisp
+++ lib/lisp/kmeans.lisp
@@ -7,62 +7,57 @@
           (.5 hey 1 true)
           (1 hey 1 false)
           (1 bye 1 false)
-          (.7 hey 1 false)
-          (.3 bye 1 true)
-          (0 bye 1 false)
           )))
 
 (defun kmeans (k)
   (let* ((tbl (xindex (test-data)))
-         (centroid-list (kmeans-find-centroid k (f tbl))) ;create a numeric list of what rows are centroids
-         (centroid0 '()) ;list for actual centroid data
-         (n 0))
-    (dolist (i centroid-list) ;this grabs the centroid data
-      (push (nth i (table-all tbl)) centroid0)) ;puts the centroid data in centroid0
+        (centroid-list (kmeans-find-centroid k (f tbl))) ;create a numeric list of what rows are centroids
+        (centroid0 '()) ;list for actual centroid data
+        (n 0))
+    (dotimes (i (length centroid-list));this grabs the centroid data
+      (push (nth (nth i centroid-list) (table-all tbl)) centroid0)) ;puts the centroid data in centroid0
+    (setf centroid0 (reverse centroid0))
     (dolist (row (table-all tbl)) ;checks all rows for distance
       (if (member n centroid-list) ;if the row being checked is already a centroid
           '(centroid) ;does nothing
           (kmeans-distance centroid0 row (table-columns tbl))) ;finds which centroid is closest to the row
-      (setf n (+ n 1)))))
-
+      (incf n))))
 
 (defun kmeans-find-centroid (k length)
   (let ((centroid-list '())) 
-    (if (<= k length) ;if there is going to be more centroids then rows of data
-        (progn
-          (dotimes (n k)
-            (let ((number (random length))) ;choose a centroid random
-              (if (member number centroid-list) ;checks if the random number is already in the list
-                  (setf n (- n 1)) ;if it was already in the list subtract 1 from n
-                  (push number centroid-list)))) ;if it wasn't already in the list push it onto the list
-          (sort centroid-list #'<))))) ;sort so I could read it better when testing it
+    (when (<= k length) ;if there is going to be more centroids then rows of data
+      (dotimes (n k centroid-list)
+        (let ((number (random length)));choose a centroid random
+          (if (member number centroid-list) ;checks if the random number is already in the list
+              (decf n) ;if it was already in the list subtract 1 from n
+              (push number centroid-list)))))));if it wasn't already in the list push it onto the list
 
 (defun kmeans-move-centroid ())
-
+                 
 (defun kmeans-distance (centroids row columns)
-  (let ((n 0)
-        (i 0)
-        (closenode 0)
+  (let ((i 0)
         (distance1 0)
-        (distance2 0))
-    (dolist (centroid centroids closenode) ;go through the list of centroids
-      (dolist (datan (eg-features row)) ;go through the data
-        (if (numericp (header-name (nth n columns))) ;if the column is numeric do number comparison
-            (setf distance1 (+ distance1 (- (* datan datan) (* (nth n (eg-features centroid)) (nth n (eg-features centroid)))))) ;a squared - b squared
-            (progn
-              (if (samep datan (nth n (eg-features centroid))) ;if the discrete data is the same then do nothing
-                  '(nothing)
-                  (setf distance1 (+ distance1 1))))) ;if it was different add 1 to the difference
-        (setf n (+ n 1))) ;counter to track which data is being checked
-      (if (= i 0) ;if it was the first centroid looked at do stuff
+        (distance2 0)
+        (close-cent 0))
+    (dolist (current-cent centroids close-cent) ;do every centroid and return which was the best
+      (let ((n 0)) ;to keep an eye on which column of data is being looked at
+        (dolist (datan (eg-features row)) ;check the rows data
+          (if (numericp (header-name (nth n columns))) ;if that column is header
+              (progn
+                (if (and (numberp datan) (nth n (eg-features current-cent))) ;makes sure that both data are numbers
+                    (setf distance1 (+ distance1 (square (- datan (nth n (eg-features current-cent)))))) ;(row - distance) squared
+                    (incf distance1))) ;if the row or the centroid had blank data or ? just say it's distance is 1
+              (progn
+                (if (samep datan (nth n (eg-features current-cent))) ;if discrete check if the symbol is the same
+                    '(nothing) ;if it is the same do nothing
+                    (incf distance1)))) ;if it is different add 1
+          (incf n))) ;increase the column being checked
+      (setf distance1 (sqrt distance1)) ;once all columns are done take the square root of it
+      (if (= i 0) ;if this was the first centroid being checked it becomes the current best
+          (setf distance2 distance1) ;distance2 is the current best
           (progn
-            (setf distance2 distance1) ;so we can compare data
-            (setf closenode 0) ;say that the first centroid was closest to the row
-            (setf i 1)) ;increase the centroid looked at by 1
-          (progn
-            (if (< distance1 distance2) ;if the new distance from the ith centroid is less then the original
-                (progn
-                  (setf closenode i) ;say that the ith centroid is closest to the row
-                  (setf i (+ i 1)) ;increase the centroid being looked at
-                  (setf distance2 distance1))))) ;set the current best centroid as distance2
-      (setf n 0)))) ;restart the data counter
+            (when (< distance1 distance2) ;if the current centroid is better then last it becomes the best
+              (setf close-cent i) 
+              (setf distance2 distance1))))
+      (setf distance1 0) ;reset the current distance
+      (incf i)))) ;do the next centroid
