25ed19277b695366f895416dfd3a858382aabc1e
willko86 <willko86@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-11 09:47:36 +0000

    discretizer added, but not working yet

---
+++ our/etc/proj/2/nbins.lisp
@@ -0,0 +1,43 @@
+(defun transpose (x)
+   (apply #'mapcar (cons #'list x)))
+
+(defun lowest (data)
+  (let ((lowval (car data)))
+    (dolist (current data lowval)
+      (if (< current lowval)
+          (setf lowval current)
+          ()
+     ))))
+
+(defun highest (data)
+  (let ((highval (car data)))
+    (dolist (current data highval)
+      (if (> current highval)
+          (setf highval current)
+          ()
+     ))))
+
+(defun nbins (n data)
+(let ((newdata (transpose data)))                                 ;make data a list of columns instead of list of rows
+   (dolist (sublist newdata newdata)                           ;traverse each column to ...
+     (if (typep (car sublist) 'number);if1              ; ...see if it contains numeric or discrete data
+         (let ((interval (/ (- (highest sublist) (lowest sublist)) n)));if numeric, generate bin size
+               (dolist (element sublist sublist)                  ;then go through each element
+                        (dotimes (x n)                          ;at each element, figure out what bin it goes in...
+                        (let((thisBin (* interval x)))       ;...by multiplying the bin size by which iteration we're on
+                             (if (< element (+ thisBin interval));if2            ;see if number fits the bin
+                                  (if (> (nth element sublist) thisBin);if3
+                                     (setf (nth element sublist) thisBin) ; 
+                                      ;else3                  
+                                    );close if3
+                                  ;else2
+                              );close if
+                         );close let, body of dotimes
+                         );close dotimes, body of dolist
+                );close dolist body
+               );close let
+        ;else1
+       );close if body
+     );close dolist
+   (transpose newdata)
+ ));close defun                                ;switch data back to a list of rows
