739d3c508d0ed921c6b2c843a687d5f10a5fc09e
zackhutzell <zackhutzell@gmail.com>
2009-10-12 23:13:54 +0000

    Added eq freq discretizer

--- our/etc/proj/2/util.lisp
+++ our/etc/proj/2/util.lisp
@@ -32,3 +32,98 @@
     (setf (table-indexed new-tbl) nil)
     new-tbl))
 
+;;Returns a list of header structures in a table structure.
+(defun get-table-column-headers (tbl)
+  (table-columns tbl))
+
+;;Returns the header structure for columni.
+(defun get-table-column-header (tbl columni)
+  (nth columni (get-table-column-headers tbl)))
+
+;;Returns a list of symbols representing the header names for each column in
+;;tbl.
+(defun get-table-column-header-names (tbl)
+  (mapcar #'header-name (get-table-columns tbl)))
+
+;;Returns a list of the values stored in columni.
+(defun get-table-column (tbl columni)
+  (mapcar #'(lambda (row) (nth columni (eg-features row))) (get-table-rows tbl)))
+
+;;Returns true if the column header for columni in tbl is of type numeric.
+(defun table-column-numericp (tbl columni)
+  (typep (get-table-column-header tbl columni) 'numeric))
+
+;;Returns true if column-header is of type numeric.
+(defun column-header-numericp (column-header)
+  (typep column-header 'numeric))
+
+;;Returns a list of eg structures representing the rows in the table structure.
+(defun get-table-rows (tbl)
+  (table-all tbl))
+
+;;Returns a list of feature lists from the table structure.
+(defun get-table-features (tbl)
+  (mapcar #'eg-features (get-table-rows tbl)))
+
+;;Modifies tbl to convert the header structure for columni into a discrete
+;;column header.
+(defun numeric2discrete (tbl columni)
+  (let ((column-header (get-table-column-header tbl columni)))
+    (setf column-header (make-discrete :name (intern (subseq (symbol-name (header-name column-header)) 1)) 
+                                                     :classp (header-classp column-header) 
+                                                     :ignorep (header-ignorep column-header) 
+                                                     :uniques nil))
+    (setf (nth columni (table-columns tbl)) column-header)
+    (dolist (row (get-table-column tbl columni))
+      (if (not (member row (discrete-uniques column-header)))
+        (push row (discrete-uniques column-header))))
+    (setf (discrete-uniques column-header) (nreverse (discrete-uniques column-header)))))
+
+(defun numeric-test-tbl ()
+  (data :name 'test
+        :columns '($test1 $test2 class)
+        :egs '((1 20 true)
+               (2  19 true)
+               (3 18 true)
+               (4 17 true)
+               (5 16 true)
+               (6 15 true)
+               (7 14 true)
+               (8 13 true)
+               (9 12 true)
+               (10 11 true)
+               (11 10 false)
+               (12 9 false)
+               (13 8 false)
+               (14 7 false)
+               (15 6 false)
+               (16 5 false)
+               (17 4 false)
+               (18 3 false)
+               (19 2 false)
+               (20 1 false))))
+
+(defun eq-width-discretized-test-tbl ()
+  (data :name 'test
+        :columns '(test1 test2 class)
+        :egs '((0 9 true)
+               (0 9 true)
+               (1 8 true)
+               (1 8 true)
+               (2 7 true)
+               (2 7 true)
+               (3 6 true)
+               (3 6 true)
+               (4 5 true)
+               (4 5 true)
+               (5 4 false)
+               (5 4 false)
+               (6 3 false)
+               (6 3 false)
+               (7 2 false)
+               (7 2 false)
+               (8 1 false)
+               (8 1 false)
+               (9 0 false)
+               (9 0 false))))
+
