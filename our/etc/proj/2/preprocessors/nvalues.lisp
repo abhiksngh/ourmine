b67c1135439fc1ce86daab27d7ce94b0bc9eb16e
kelcecil <kelcecil@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-12 04:05:37 +0000

    added ability to build large datasets from a mixture of tables in memory and functions.

--- our/etc/proj/2/preprocessors/nvalues.lisp
+++ our/etc/proj/2/preprocessors/nvalues.lisp
@@ -3,7 +3,9 @@
         returnTest
         columns)
   (dolist (per-func func (values (make-train-data columns returnTrain) (make-test-data columns returnTest)))
-    (multiple-value-bind (train test) (funcall per-func n)
+    (multiple-value-bind (train test) (if (tablep per-func)
+                                          (seg-dataset per-func n)
+                                          (funcall per-func n))
       (let ((trainSet (egs train))
             (testSet (egs test)))
       (dolist (per-train trainSet)
@@ -11,6 +13,17 @@
       (dolist (per-test testSet)
         (push (eg-features per-test) returnTest))
     (setf columns (columns-header (table-columns train))))))))
+
+(defun seg-dataset (table &optional (n 0.3))
+  (let* (train
+         test
+         (k (* n (length (table-all table)))))
+    (dolist (per-instance (shuffle (table-all table)))
+      (if (> (decf k) 0)
+          (push (eg-features per-instance) test)
+          (push (eg-features per-instance) train)
+          ))
+    (values (make-train-data (columns-header (table-columns table)) train) (make-test-data (columns-header (table-columns table)) test))))
 
 (defun make-train-data (cols eg)
   (data
