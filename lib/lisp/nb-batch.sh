3682edbada5033fef059c6fa96faea88fd5fcc87
Adam.M.Brady <Adam.M.Brady@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-13 21:03:01 +0000

    all done

--- lib/lisp/nb-batch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-for file in ./proj2/HyperPipes/NBData/*.lisp
-do
-file=`basename $file`	#remove directory
-file=${file%.lisp}	#remove .lisp
-echo $file
-sbcl --load nb-batch.lisp --eval "(nb-batch \"$file\")"
-done
--- lib/lisp/proj2/HyperPipes/makedata.sh
+++
@@ -1,4 +0,0 @@
-for file in OutputFiles/*.txt; do
-base=`basename $file`
-gawk -f stats.awk -v Filename=$file > PlotData/$base
-done
