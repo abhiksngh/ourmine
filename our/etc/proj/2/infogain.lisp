e5e7f60760e689d61076c0f75098523b5c50940c
explodeyourself <explodeyourself@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-14 04:30:06 +0000

    infogain working.  Cleaning up print statements

--- our/etc/proj/2/infogain.lisp
+++ our/etc/proj/2/infogain.lisp
@@ -16,10 +16,12 @@
   (let* ((all (remove-duplicates column))
          (sum 0)
          (prob)
+         (new-header)
+         (new-columns)
          )
     (dolist (item all)
       (setf prob (probability item column))
-      (setf sum (+ sum (* (* prob prob) (log prob))))
+      (setf sum (+ sum (* prob (i prob))))
 ;      (print sum)))
       )
     sum))
@@ -42,22 +44,68 @@
   (let* ((table (copy-table source-table))
          (gain-lst)
          (max-gain)
+         (min-allowed-gain)
          (new-table)
+         (new-header)
+         (new-data)
          (transposed-data (transpose (get-data source-table)))
          )
+
+    
+    
 ;get info-gain for all columns
     (dolist (one-column transposed-data)
       
       (setf gain-lst (append gain-lst (list (compute-infogain one-column))))
       )
-
-    (setf max-gain (max-list gain-lst))
     
-    max-gain))
 
+    ;get the max gain
+    (setf max-gain (max-list gain-lst))
+
+    ; get minimum gain
+    (setf min-allowed-gain (* max-gain (/ percent 100)))
+
+    ;for width of table
+
+    (dotimes (n (table-width table))
+      ;if the current gain is above the min-allowed-gain
+      ;Keep
+      (print min-allowed-gain)
+      (print "versus")
+      (print (nth n gain-lst))
+      (print (< min-allowed-gain (nth n gain-lst)))
+      (print " ")
+      
+      
+      (if (< min-allowed-gain (nth n gain-lst))
+          (progn
+            (print "found less")
+                                        ;Add to new header
+                                        ;Add column to new data
+            
+            (setf new-header (append new-header (list (nth n (columns-header (table-columns source-table))))))
+            (setf new-data (append new-data (list (nth n transposed-data))))
+            )
+          )
+      )
+      (print "new-header")
+      (print new-header)
+      (print (transpose new-data))
+      (setf new-table (data
+                      :name (table-name table)
+                      :columns new-header
+                      :klass (table-class table)
+                      :egs
+                      (transpose new-data)))
+      ))
 
 (defun max-list (lst)
   (let ((current-max (first lst)))
     (dolist (item lst)
       (setf current-max (max current-max item)))
     current-max))
+
+
+(defun i (probability)
+  (* (- 0 probability) (log probability 2)))
