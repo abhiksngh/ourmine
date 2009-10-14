211fc52808777425d946d53fd3f5997ce5534a3b
bigyums3 <bigyums3@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-14 15:57:48 +0000

    b-squared

---
+++ lib/lisp/b-squared.lisp
@@ -0,0 +1,12 @@
+(defun test-data()
+  (data
+   :name 'test
+   :columns '($number1 symbol $number2 stuff)
+   :egs '((0 bye 1 true)
+          (.2 hey 1 true)
+          (.5 hey 1 true)
+          (1 hey 1 false)
+          (1 bye 1 false)
+          )))
+          
+(defun b-squared ()
