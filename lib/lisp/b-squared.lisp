5b392b016f6fe2de56a4d6c398b8395c36b0c1f2
bigyums3 <bigyums3@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-14 18:46:51 +0000

    update

--- lib/lisp/b-squared.lisp
+++ lib/lisp/b-squared.lisp
@@ -1,12 +1,9 @@
-(defun test-data()
-  (data
-   :name 'test
-   :columns '($number1 symbol $number2 stuff)
-   :egs '((0 bye 1 true)
-          (.2 hey 1 true)
-          (.5 hey 1 true)
-          (1 hey 1 false)
-          (1 bye 1 false)
-          )))
-          
-(defun b-squared ()
+
+(defun b-squared (data)
+	(let* (score-data(data))					     ;take the data and set utility values
+		(sort-scored(score-data data))				     ;sort the utility values for chopping
+		(chop-data(sort-scored(score-data data)))		     ;chop the data into the best and rest catergory
+		(find-range(chop-data(sort-scored(score-data data))))))      ;find and sort range with b^2 / (b+r) formula
+		
+(defun score-data(data)
+	(let * 
