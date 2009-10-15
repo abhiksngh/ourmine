5acb48a94523a4e244398b56b51152dd0025f64c
menzies.tim <menzies.tim@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-15 03:05:11 +0000

    stuff

--- our/lib/lisp/tricks/list.lisp
+++ our/lib/lisp/tricks/list.lisp
@@ -6,3 +6,22 @@
 
 (defun head (l) (car l))
 (defun tail (l) (first (last l)))
+
+
+(deftest  test-head ()
+  (check
+    (eql (head '(aa bb cc)) 'aa)))
+
+(defun ourmember (x l)
+  (if (null l)
+      nil
+      (or (eql x (first l))
+	  (ourmember x (rest l)))))
+
+(defun ourmember1 (x l)
+  (and l
+       (or 
+	(eql x (first l))
+	(ourmember x (rest l)))))
+
+
