30160345bf12eee87332579cb0654d5c75fe029d
bigyums3 <bigyums3@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-20 00:54:00 +0000

    updated b^2

--- lib/lisp/b-squared.lisp
+++ lib/lisp/b-squared.lisp
@@ -1,9 +1,17 @@
 
-(defun b-squared (data)
-	(let* (score-data(data))					     ;take the data and set utility values
+(defun b-squared (data className)
+	(let* (score-data(data className))				     ;take the data and set utility values
 		(sort-scored(score-data data))				     ;sort the utility values for chopping
 		(chop-data(sort-scored(score-data data)))		     ;chop the data into the best and rest catergory
 		(find-range(chop-data(sort-scored(score-data data))))))      ;find and sort range with b^2 / (b+r) formula
 		
-(defun score-data(data)
-	(let * 
+(defun score-data(data className)					    ;takes the data table and the class name to examine
+	(let* clas table-class))					    ;makes a class variable for the table data
+	(dotimes (i 							    ;iterates through the class column to examine datums
+	(if (eql clas className)					    ;checks class in table versus input name
+		((cons 'listBest)                                           ;builds best and rest lists
+		   (incf bestCtr))
+		((cons 'listRest)
+		    (incf restCtr)))
+
+		
