92fc4449d36333697348c7a45497c2072aaa121a
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-20 08:34:22 +0000

    Adding stuff for GENIC rewrite.

--- etc/proj/2/sandbox.lisp
+++ etc/proj/2/sandbox.lisp
@@ -1,114 +1,72 @@
-;;; Delete this at your own peril, for it will beckon towards death. -Drew
-
 (load "lib/loaddeps")
 (load "utils/utils")
 (load "d-data/mushroom")
-(load "algos/genic")
-
 (defparameter mushtable (mushroom))
 
-(defun unnegitify (possibly-negative)
-  (if (< possibly-negative 0)
-    0
-    possibly-negative
-  )
-)
-
-(defun unoverbound (possibly-toobig table)
-  (if (>= possibly-toobig (length (table-all table)))
-    (- (length (table-all table)) 1)
-    possibly-toobig
-  )
-)
-
-(defun closest-member (table row-number find-same-class)
-  (let ((target-class (eg-class (nth row-number (table-all table)))))
-    (loop for i from 1 to (length (table-all table)) do
-      (let ((left (nth (unnegitify (- row-number i)) (table-all table))) (right (nth (unoverbound (+ row-number i) table) (table-all table))))
-        (if (and (not (null (eg-class left))) (equalp (eg-class left) target-class))
-          (if find-same-class 
-            (return (unnegitify (- row-number i)))
-          )
-          (if (and (not (null (eg-class right))) (not find-same-class))
-            (return (unnegitify (- row-number i)))
-          )
-        )
-        (if (and (not (null (eg-class right))) (equalp (eg-class right) target-class))
-          (if find-same-class
-            (return (unoverbound (+ row-number i) table))
-          )
-          (if (and (not (null (eg-class right))) (not find-same-class))
-            (return (unoverbound (+ row-number i) table))
-          )
-        )
-      )
+(defun new-random-center (table &optional (num-needed 1))
+  (let ((newclusters nil) (num-fields (length (table-all table))))
+    (dotimes (n num-needed newclusters)
+      (setf newclusters (append (list (nth (random num-fields) (table-all table))) newclusters))
     )
   )
 )
 
-(defun relief-diff (column-num target neighbor &optional (divisor-n 1))
-  (let* ((score 0) (target-feature (nth column-num (eg-features target))) (neighbor-feature (nth column-num (eg-features neighbor))))
-    (if (and (realp target-feature) (realp neighbor-feature))
-      (setf score (+ score (/ (abs (- target-feature neighbor-feature)) (+ target-feature neighbor-feature))))
-      (if (equalp target-feature neighbor-feature)
-        (setf score (+ score 1))
-      )
+(defun fetch-generation (table gen-size generation)
+  (let ((start (* gen-size generation)) (end (* gen-size (+ 1 generation))) (subset nil))
+    (loop for n from start to end do
+      (setf subset (append (list (nth n (table-all table))) subset))
     )
-    (/ score divisor-n)
+    subset
   )
 )
 
-(defun relief-col-weights (table &optional (iterations nil))
-  (if (null iterations)
-    (setf iterations 250)
-  )
-  (let ((column-weights (make-list (length (eg-features (first (table-all table)))) :initial-element 0)))
-    (loop for i from 0 to iterations do
-      (let* ((record-num (random (length (table-all table)))) (hit (closest-member table record-num t)) (miss (closest-member table record-num nil)))
-        (loop for j from 0 to (- (length column-weights) 1) do
-          (let 
-              ((round-score-hit (relief-diff j (nth record-num (table-all table)) (nth hit (table-all table)) iterations)) 
-               (round-score-miss (relief-diff j (nth record-num (table-all table)) (nth miss (table-all table)) iterations)))
-            (setf (nth j column-weights) (- (+ (nth j column-weights) round-score-miss) round-score-hit))
-          )
-        )
-      )
-    )
-    column-weights
-  )
-)
-
-(defun relief (table)
-  (let* ((weights (relief-col-weights table)) (sorted-weights (sort (copy-list weights) #'>)) (threshold (nth (round (/ (length weights) 3)) sorted-weights)) (newtable nil))
-    (setf 
-        newtable (make-table)
-        (table-name newtable) (table-name table)
-        (table-class newtable) (table-class table)
-        (table-cautions newtable) (table-cautions table)
-        (table-indexed newtable) (table-indexed table)
-    )
-    (let ((column-names (table-columns table)) (new-cols nil))
-      (loop for j from 0 to (- (length sorted-weights) 1) do
-        (if (>= (nth j weights) threshold)
-          (setf new-cols (append new-cols (list (nth j column-names))))
-        )
-      )
-      (setf (table-columns newtable) new-cols)
-    )
-    (let ((all-rows (table-all table)))
-      (dolist (x all-rows)
-        (let ((old-cols (eg-features x)) (new-cols nil))
-          (loop for k from 0 to (- (length (eg-features x)) 1) do
-            (if (>= (nth k weights) threshold)
-              (setf new-cols (append new-cols (list (nth k old-cols))))
+(defun closest-center (centers new-point)
+  (let ((distances (make-list (length centers) :initial-element 0)))
+    (dotimes (i (length centers))
+      (dotimes (j (length (eg-features new-point)))
+        (if (numericp (nth j (eg-features new-point)))
+          (setf 
+            (nth i distances) 
+            (+ 
+              (nth i distances) 
+              (sqrt (expt (- (nth j (eg-features new-point)) (nth j (eg-features (nth i centers)))) 2))     
             )
           )
-          (let ((new-record (make-eg :features new-cols :class (eg-class x))))
-            (setf (table-all newtable) (append (table-all newtable) (list new-record)))
+          (if (not (equalp (nth j (eg-features new-point)) (nth j (eg-features (nth i centers)))))
+            (incf (nth i distances))
           )
         )
       )
     )
-    newtable
+    (let ((shortest-dist nil) (shortest-pos nil))
+      (dotimes (k (length distances))
+        (if (or (null shortest-dist) (< (nth k distances) shortest-dist))
+          (setf shortest-dist (nth k distances) shortest-pos k)
+        )
+      )
+      shortest-pos
+    )
   )
+)
+
+(defun move-center (existing-center new-point weight)
+  (let ((existing-features (copy-list (eg-features existing-center))) (new-features (eg-features new-point)))
+    (dotimes (i (length existing-features))
+      (let ((col-val-old (nth i existing-features)) (col-val-new (nth i new-features)))
+        (if (numericp col-val-old)
+          (setf
+            (nth i existing-features)
+            (/ (+ (* weight col-val-old) col-val-new) (+ weight 1))
+          )
+          (if (and (not (equalp col-val-old col-val-new)) (equalp weight 1))
+            (setf (nth i existing-features) col-val-new)
+          )
+        )
+      )
+    )
+  )
+)
+
+(defun cull-centers (centers weights)
+  centers
 )
