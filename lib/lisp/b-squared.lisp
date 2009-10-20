58770dbdd079af51307e6b46b865737d1f90f160
bigyums3 <bigyums3@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-10-20 02:43:15 +0000

    more b to the squared

--- lib/lisp/b-squared.lisp
+++ lib/lisp/b-squared.lisp
@@ -1,12 +1,13 @@
 
-(defun b-squared (data className)
-	(let* (score-data(data className)				     ;take the data and set utility values
+(defun b-squared (table className)
+	(let* data (table-copy(data table))
+	        (score-data(data className)				     ;take the data and set utility values
 		(sort-scored(score-data data)				     ;sort the utility values for chopping
 		(find-range(score-data data))		    
 		(sort-scored (find-range(score-data data)))                           ;find and sort range with b^2 / (b+r) formula
 		
 (defun score-data(data className)					    ;takes the data table and the class name to examine
-	(let* clas table-class))					    ;makes a class variable for the table data
+	(let* (clas table-class))					    ;makes a class variable for the table data
 	(dotimes (i 							    ;iterates through the class column to examine datums
 	(if (eql clas className)					    ;checks class in table versus input name
 		((cons 'listBest)                                           ;builds best and rest lists
@@ -22,5 +23,12 @@
 			(let rangeBest 0))
 			
 (defun sort-scored(find-range(score-data data className))
-	(let (scoredBest (sort listBest #'))
-		(scoredRest (sort listRest #')))
+	(let*   (medianScore (/ (* best best) (+ best rest)))
+		(scoredBest (sort rangeBest #'< medianScore))
+		(scoredRest (sort rangeRest #'< medianScore))
+	(append scoredBest scoredRest)
+	(loop for x in scoredBest
+		do (if (scoredBest 0.8 #'>)
+			(push x result)))))
+	
+	
