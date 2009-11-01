cbc2a150c5850e3a4c6eac799441f30a61553fae
edwaltman <edwaltman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-11-01 22:53:16 +0000

    Progress on Normal Binning.

--- etc/proj/3/algos/normalbins.lisp
+++ etc/proj/3/algos/normalbins.lisp
@@ -1,6 +1,32 @@
 ;;; Claimee: Drew
 
 ;;;  (EASY) Normal-chops
-;;;    Assume data is Gaussian and divide into -3, -2, -1, 0, 1,2,3 standard deviations away from the mean. Hint: see lisp/tricks/normal.lisp
+;;;    Assume data is Gaussian and divide into -3, -2, -1, 0, 1,2,3 standard 
+;;;    deviations away from the mean. Hint: see lisp/tricks/normal.lisp
 
+;;; 1. Compute the mean of a column.
 
+(defun column-mean (table column-num)
+)
+
+;;; 2. Compute the standard deviation of a column.  StDev is measured as
+;;;    the average of the distances of each value from the mean.  Relevant
+;;;    equation is: stddev = sqrt(1/n * Summation((member - mean)^2))
+
+(defun column-stddev (table column-num)
+)
+
+;;; 3. Generate bin widths, return a list of staring and ending values
+;;;    in the format of ((-20 -10) (-10 0) (0 10)) and so forth.  When
+;;;    doing binning, the lower value is inclusive, the upper value
+;;;    is not, so >= car and < cdar meets critera for binning.
+
+(defun column-binwidths (table column-num)
+)
+
+;;; 4. For each numeric column in the table, compute average, then
+;;;    standard deviation, then compute bin ranges, then overwrite
+;;;    column values with their new bin number.
+
+(defun normal-bins (table)
+)
