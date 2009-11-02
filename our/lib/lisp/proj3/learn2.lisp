24db6ce352851c2ff4b13d3e0adf4cd54b36bb6b
RabiTuckman <RabiTuckman@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-11-02 19:54:47 +0000

    fixing to use funcall wootages

--- our/lib/lisp/proj3/learn2.lisp
+++ our/lib/lisp/proj3/learn2.lisp
@@ -1,15 +1,14 @@
 
 ;testing infogain and NB learner 
-(defun infogain-nb(train n)
-  (classify-nb (infogain-table (discretize train) n)))
+(defun infogain-nb (train n)
+  (classify-learner 'bayes-classify-num (infogain-table (discretize train) n)))
 
 ;tesing  bsquared and NB
 (defun bsquare-nb (train n)
-  (classify-nb (bsquare train n)))
+  (classify-learner 'bayes-classify-num train))
 
-
-;;classfying infogain and NB learner
-(defun classify-nb (train)
+;;classfying a learner 
+(defun classify-learner (fun train)
   (let* ((class (table-class train))
          (lst (split2bins train))
          (train (xindex (car (cdr lst))))
@@ -17,7 +16,7 @@
          (gotwants))
     (dolist (test_inst (get-features (table-all test)))
       (let* ((want (nth class test_inst))
-             (got (bayes-classify-num test_inst train)))
+             (got (funcall fun test_inst train)))
         (setf want (list want))
         (setf gotwants (append gotwants (list (append want got)))))) 
     (split (abcd-stats gotwants :verbose nil))))
