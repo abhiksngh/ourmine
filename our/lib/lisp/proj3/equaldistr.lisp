49a58cd02a0dfa3c25dede5a7686b879ec8b282d
w.w.mensah <w.w.mensah@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-11-03 01:28:39 +0000

    equal distribution table

---
+++ our/lib/lisp/proj3/equaldistr.lisp
@@ -0,0 +1,27 @@
+(defun equaldistr2 (train)
+  (let* ((data (table-egs-to-lists train))
+         (class-col (table-class train))
+         (temp-inst)
+         (count 0)
+         (t-count (f (xindex train) 'true))
+         (f-count (f (xindex train) 'false)))
+    (if (> t-count f-count)
+        (progn
+          (loop do
+               (loop do
+                    (setf temp-inst (nth (random (length data)) data))
+                  until (eql (nth class-col temp-inst) 'true))
+               (setf data (remove temp-inst data :test #'equal))
+               (incf count)
+             until (= (length data) (* f-count 2))))
+        (progn
+          (loop do
+               (loop do
+                    (setf temp-inst (nth (random (length data)) data))
+                  until (eql (nth class-col temp-inst) 'false))
+               (setf data (remove temp-inst data :test #'equal))
+               (incf count)
+             until (= (length data) (* t-count 2)))))
+    data))
+        
+
