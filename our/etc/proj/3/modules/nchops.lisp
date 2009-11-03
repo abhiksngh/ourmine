bd25ddd3bac8f62b1be055ff9f1c361b179cca02
mparsons6 <mparsons6@gmail.com>
2009-11-03 07:14:21 +0000

    n-bins too?

--- our/etc/proj/3/modules/nchops.lisp
+++ our/etc/proj/3/modules/nchops.lisp
@@ -1,18 +1,14 @@
-; TODO
-
-
+; n-chops
+; n-bins
 (defun n-chops (tbl &optional (n 8))
   "Equal-frequency discretization with N bins"
   (let ((colnum 0)
 	(binsize (/ (negs tbl) n)))
     (dolist (column (table-columns tbl) tbl)
-;      (when (and (typep column 'numeric)
-;		 (null (header-classp column))) ; MP: Should we run on the class too?
       (when (typep column 'numeric)
-	(setf tbl (sort-on-gen tbl colnum :comp #'>))
 	(let ((binname 1)
 	      (bincount 0))
-	  (dolist (record (table-all tbl))
+	  (dolist (record (table-all (sort-on-gen tbl colnum :comp #'>)))
 	    (incf bincount)
 	    (when (>= bincount binsize)
 	      (incf binname)
@@ -21,19 +17,19 @@
 	(setf (nth colnum (table-columns tbl)) (discrete-column column)))
       (incf colnum))))
 
-
 (defun n-bins (tbl &optional (n 8))
   "Equal-width discretization with N bins"
   (let ((colnum 0))
-    (dolist (column (table-columns tbl))
+    (dolist (column (table-columns tbl) tbl)
       (when (typep column 'numeric)
 	(let* ((norm (fill-normal tbl colnum))
-	       (binwidth (/ (- (normal-max norm) (normal-min norm)) n)))
-	  (dolist (record (table-all tbl))
+	       (binwidth (/ (- (normal-max norm) (normal-min norm)) n))
+	       (binname 0))
+	  (dolist (record (table-all (sort-on-gen tbl colnum :comp #'>)))
 	    (setf (nth colnum (eg-features record))
 		  (round (* n (/ (- (nth colnum (eg-features record))
 				    (normal-min norm))
 				 (- (normal-max norm)
-				    (normal-min norm)))))))))
-      (incf colnum)))
-  (sort-on-gen tbl 0 :comp #'>)) ; move return to dolist
+				    (normal-min norm))))))))
+	(setf (nth colnum (table-columns tbl)) (discrete-column column)))
+      (incf colnum))))
