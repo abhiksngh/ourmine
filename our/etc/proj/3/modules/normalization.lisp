0058c71a8ef56722259b44e7d29f9d95142188aa
sr.wvu1 <sr.wvu1@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-11-03 02:21:22 +0000

    added normalize pre-processor

---
+++ our/etc/proj/3/modules/normalization.lisp
@@ -0,0 +1,10 @@
+; Implements the Normalize pre-processor.
+(defun normalization (tbl)
+  (let ((normval)
+        (n 0))
+    (dolist (column (table-columns tbl) tbl)
+      (when (typep column 'numeric)
+        (setf normval (fill-normal tbl n))
+        (dolist (record (table-all tbl))
+          (setf (nth n (eg-features record)) `(,(normalize normval (nth n (eg-features record)))))))
+      (incf n))))
