bc8a97f993c6a8ce3af4ec9f4faa41f284793e07
menzies.tim <menzies.tim@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-11-03 20:42:34 +0000

    commit log

--- our/etc/proj/3/modules/nomograms.lisp
+++ our/etc/proj/3/modules/nomograms.lisp
@@ -1,3 +1,13 @@
 (defun nomograms (tbl &key (percentile .20) (threshold .25))
   (b-squared tbl :percentile percentile :threshold threshold
              :score (lambda (b r) (/ (log b) (log r)))))
+
+
+(defun nomograms1 (tbl &key (percentile .20) (threshold .25))
+  (b-squared tbl :percentile percentile :threshold threshold
+             :score (lambda (b r) (if (> b r) 
+				      (log (/ b r))
+				      0))))
+
+(defun nomograms2 (tbl &key (percentile .20) (threshold .25))
+  (b-squared tbl :percentile percentile :threshold threshold))
