9edcacfe362ad542fb0857c8c02d494078fa5fab
timbielawa <timbielawa@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-11-04 05:31:19 +0000

    saving before going home

--- etc/proj/3/algos/infogain.lisp
+++ etc/proj/3/algos/infogain.lisp
@@ -1,2 +1,7 @@
 ;; Infogain
 ;; Implemented as Feature Subset Selection
+
+
+;; Calculate the information of each column.
+;; Drop a column if the information gain is less than "X%" of the maximum informaion gain.
+
