31972fb4037781cb8dd5d2de150dca698860b087
timbielawa <timbielawa@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-11-04 00:21:43 +0000

    2b and normalize

---
+++ etc/proj/3/algos/2b.lisp
@@ -0,0 +1,8 @@
+;; 2b
+;; Pre-Processor to Naive Bayes.
+;;; Shaggy
+;; Requires twor.lisp and naivebayes.lisp be loaded.
+;; The pair-table macro returns an xindex'd table of all the rows paired.
+
+(defun 2b (tbl)
+  (naivebayes (pair-table tbl)))
---
+++ etc/proj/3/algos/normalize.lisp
@@ -0,0 +1,4 @@
+;; Normalize
+;; Normalize numerics from 0...1 
+;;; Shaggy
+
