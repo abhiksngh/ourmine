b45f05ca42333cf2ccdf29d93c206f8ae535e252
mparsons6 <mparsons6@gmail.com>
2009-11-20 05:58:10 +0000

    We produce a good rule and quick enough, though I'm tempted to write another version and compare once I'm finished. Now to get the right output.

--- our/etc/proj/3/modules/oner.lisp
+++ our/etc/proj/3/modules/oner.lisp
@@ -1,4 +1,12 @@
+(defstruct bestrule
+  column   ; The column name.
+  attval   ; Attribute value.
+  classval ; Class value.
+  count    ; Number of correct predictions.
+  )
+
 (defun add-rule (ht attribute class)
+  "Add a rule into the hash structure."
   (let ((outer (gethash attribute ht))
 	inner)
     (when (null outer)
@@ -8,18 +16,34 @@
       (setf inner (setf (gethash class outer) 0)))
     (setf inner (incf (gethash class outer)))))
 
+(defun rule-print (hash &optional (prefix ""))
+  "A debug print function."
+  (maphash #' (lambda (key val)
+		(format t "~A~A = ~A~%" prefix key val)
+		(when (typep val 'HASH-TABLE)
+		  (rule-print val "->"))) hash))
+
+(defun best-rule (hash best col)
+  "Determine the best rule for a column."
+  (maphash #' (lambda (attval classht)
+		(maphash #' (lambda (classval count)
+			      (when (< (bestrule-count best) count)
+				(setf best (make-bestrule :column col :attval attval :classval classval :count count))))
+			    classht))
+	      hash)
+  best)
+
 (defun oner (table &optional features)
-  (let ((rows (length (table-all table)))
+  (let ((ht (make-hash-table :size (/ (length (table-all table)) 3)))
 	colindex
-	best)
-    ; This could be more efficient of indexof is replaced (I think - check later).
+	(best (make-bestrule :column nil :attval nil :classval nil :count 0)))
+    ; This could be more efficient if indexof is replaced (I think - check later).
     (dolist (column (if (null features) (table-columns table) features))
-      (setf colindex (indexof column (table-columns table)))
-      ; MP: I think this might be a good size, but I'm open to suggests (or leaving it default).
-      (let ((ht (make-hash-table :size (/ rows 2))))
+      (when (null (header-classp column))
+	(setf colindex (indexof column (table-columns table)))
+        ; MP: I think this might be a good size, but I'm open to suggests (or leaving it default).
 	(dolist (record (table-all table))
 	  (add-rule ht (nth colindex (eg-features record)) (eg-class record)))
-	(maphash #'(lambda (key val)
-		     (format t "~A = ~A~%" key val)) ht)
-	(format t "~A~%" ht))
-      (return-from oner))))
+	(setf best (best-rule ht best column))
+	(format t "BEST: ~A~%" best)
+	(clrhash ht)))))
