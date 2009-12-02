abe88b3d738551805516fc6966c7c466ee1f114c
kelcecil <kelcecil@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-12-02 14:55:15 +0000

    deftest written from that snazzy webpage Menzies gave me.

--- our/etc/proj/2/stats/wilcoxian.lisp
+++ our/etc/proj/2/stats/wilcoxian.lisp
@@ -1,26 +1,43 @@
-(defun wilcoxon(pop1 pop2 level)
+(defun wilcoxon(pop1 pop2 &optional (level 0.05))
   (let* ((diff (make-list (length pop1)))
          (absDiff (make-list (length pop1)))
          (n 0))
     (doitems (onePop i pop1)
       (incf n)
       (setf (nth i diff) (- onePop (nth i pop2)))
-      (setf (nth i absDiff) (abs (- onePop (nth i pop2)))))
+      (setf (nth i absDiff) (abs (nth i diff))))
     (let ((ranks (rank absDiff))
            (w0 0)
            (w 0))
       (doitems (oneAbsDiff i absDiff)
         (setf w0 (gethash (nth i absDiff) ranks))
-        (setf w (+ w (if (< (nth i diff) 0)
-                 (* -1 w0)
-                 0))))
+        (setf w
+              (+ w
+                 (if (< (nth i diff) 0)
+                     (* -1 w0)
+                     w0))))
       (let* ((sigma-value (sigma n))
              (z-value (z w sigma-value)))
+        (print z-value)
         (if (and
              (>= z-value 0)
              (<= z-value (z-critical level)))
             NIL
             T)))))
+
+(deftest test-wilcoxon()
+  (check (equal
+          (wilcoxon
+           (list 78 24 64 45 64 52 30 50 64 50 78 22 84 40 90 72)
+           (list 78 24 62 48 68 56 25 44 56 40 68 36 68 20 58 32)
+           0.05)
+          T))
+  (check (equal
+          (wilcoxon
+           (list 78 24 64 45 64 52 30 50 64 50 78 22 84 40 90 72)
+           (list 78 24 62 48 68 56 25 44 56 40 68 36 68 20 58 32)
+           0.01)
+          NIL)))
             
 (defun sigma(n)
   (sqrt (/ (* n (+ n 1)(+ (* 2 n) 1)) 6)))
