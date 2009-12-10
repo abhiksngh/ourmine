0a905e545bbd2717dab44c601ef42b7c005d1b3f
w.w.mensah <w.w.mensah@c7096c74-56b8-11de-b31f-81d54249e7d9>
2009-12-10 17:05:43 +0000

    results pd/pf for prism

---
+++ our/lib/lisp/proj4/res2.lisp
@@ -0,0 +1,38 @@
+(defparameter final-rules '((TRUE ((4 5)) ((3 7)) ((4 3)) ((0 8)) ((0 3)) ((1 4))
+                             ((0 1) (3 1) (4 1) (1 0) (2 0)) ((3 2) (4 2) (0 1) (1 0) (2 0))
+                             ((4 1) (0 0) (3 1) (1 0) (2 0)) ((3 0) (4 0) (0 0) (1 0) (2 0)))                  
+                            (FALSE ((3 8)) ((3 0) (4 1)) ((3 0) (0 0) (1 0) (2 0) (4 0)) ((3 1) (4 0))
+                             ((4 2) (3 3)) ((4 1) (3 2)) ((3 1) (4 2)) ((3 1) (0 0) (1 0) (2 0) (4 1))
+                             ((0 1) (1 0) (2 0) (3 1) (4 1)) ((4 2) (0 1) (1 0) (2 0) (3 2))
+                             ((4 4) (0 4)))))
+
+        
+
+results
+PRISM - pd
+
+shared-ar3: 0.2857149
+shared-ar4: 0.38888904
+shared-ar5: 0.5999996
+
+turkish-shared_pc1: 1.5384612e-7
+turkish-shared_mw1: 0.42105272
+turkish-shared_mc2: 0.14634164
+turkish-shared_kc3: 0.24324338
+turkish-shared_kc2: 0.0212767
+turkish-shared_kc1: 0.094420634
+turkish-shared_cm1: 0.07500021
+
+PRISM - pf
+
+shared-ar3: 0.04347846
+shared-ar4: 1.2195119e-7
+shared-ar5: 0.090909466
+
+turkish-shared_pc1: 0.0030303132
+turkish-shared_mw1: 0.103942685
+turkish-shared_mc2: 0.052631676
+turkish-shared_kc3: 0.02067186
+turkish-shared_kc2: 2.4390243e-8
+turkish-shared_kc1: 0.010539373
+turkish-shared_cm1: 0.009803946
