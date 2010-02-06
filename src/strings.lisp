bd3bfc9633cee9cec7c016337727322f621d773c
menzies.tim <menzies.tim@c7096c74-56b8-11de-b31f-81d54249e7d9>
2010-02-06 04:01:05 +0000

    stuff

--- src/strings.lisp
+++
@@ -1,7 +0,0 @@
-(defun samep (x  y)
-  (labels  ((white (c)
-	      (member c '(#\Space #\Tab #\Newline #\Page) :test #'char=))
-	    (whiteout (s)
-	      (remove-if #'white  s)))
-    (string= (whiteout (format nil "~(~a~)" x))
-	     (whiteout (format nil "~(~a~)" y)))))
